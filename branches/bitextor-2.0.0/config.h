ac9c2b2913968da70841bd5ff86989204d48908b
espla <espla@96836ec2-454e-0410-a250-9ee83a685b91>
2008-10-01 20:13:50 +0000

    Versió corregida de Bitextor 2.0

---
+++ branches/bitextor-2.0.0/config.h
@@ -0,0 +1,157 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if the `closedir' function returns void instead of `int'. */
+/* #undef CLOSEDIR_VOID */
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#define HAVE_DIRENT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `enca' library (-lenca). */
+#define HAVE_LIBENCA 1
+
+/* Define to 1 if you have the `tagaligner' library (-ltagaligner). */
+#define HAVE_LIBTAGALIGNER 1
+
+/* Define to 1 if you have the `textcat' library (-ltextcat). */
+#define HAVE_LIBTEXTCAT 1
+
+/* Define to 1 if you have the `tidy' library (-ltidy). */
+#define HAVE_LIBTIDY 1
+
+/* Define to 1 if you have the `xml++-2.6' library (-lxml++-2.6). */
+#define HAVE_LIBXML___2_6 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the `mkdir' function. */
+#define HAVE_MKDIR 1
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if your system has a GNU libc compatible `realloc' function,
+   and to 0 otherwise. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if `stat' has the bug that it succeeds when given the
+   zero-length file name argument. */
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+   slash. */
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "miquelespla@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "bitextor"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "bitextor 2.0rc1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "bitextor"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.0rc1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef was allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef was allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to the type of a signed integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int32_t */
+
+/* Define to the type of a signed integer type of width exactly 8 bits if such
+   a type exists and the standard includes do not define it. */
+/* #undef int8_t */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
---
+++ branches/bitextor-2.0.0/src/Bitextor.cpp
@@ -0,0 +1,149 @@
+#include "WebSite.h"
+#include "FilePreprocess.h"
+#include "Bitext.h"
+#include "GlobalParams.h"
+#include "DownloadMod.h"
+#include <fstream>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+string
+GetFileName(string path)
+{
+	string exit;
+	size_t found;
+
+	found=path.find_last_of("/\\");
+	exit=path.substr(found+1);
+
+	return exit;
+}
+
+string
+GetFilePath(string path)
+{
+	string exit;
+	size_t found;
+
+	found=path.find_last_of("/\\");
+	exit=path.substr(0,found+1);
+
+	return exit;
+}
+
+int
+main (int argc, char *const *argv)
+{
+	WebSite ws;
+	vector<Bitext> results;
+	unsigned int n_results;
+	Bitext bitext;
+	ifstream file;
+	unsigned int i;
+	ostringstream aux_sstream;
+	string file_name;
+	bool show_howtouse=false;
+	string dest_dir;
+	
+	if(argc==2){
+		show_howtouse=true;
+		if(strcmp(argv[2],"-h")!=0)
+			cout<<"The selected option is not correct."<<endl;
+	}
+	else{
+		if(argc<4)
+			show_howtouse=true;
+		else{
+			try{
+				if(!GlobalParams::LoadGlobalParams(argv[1]))
+					cerr<<"Bitextor could'nt open the config file."<<endl;
+				else{
+					if(strcmp(argv[2],"-d")==0){
+						cout<<"Inicialising Bitextor's destination path..."<<endl;
+						dest_dir=argv[3];
+						if(dest_dir[dest_dir.length()-1]!='/')
+							dest_dir+="/";
+						if(!ws.Initialize(dest_dir))
+							cerr<<"There were an error while trying to load the files in the selected directory."<<endl;
+						else{
+							mkdir(((string)argv[3]+"bitexts/").c_str(),0777);
+							cout<<"Comparing the files..."<<endl;
+							results=ws.GetMatchedFiles();
+							for(n_results=0;n_results<results.size();n_results++){
+								bitext=results[n_results];
+								
+								file.open(((string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+".tmx").c_str(),ios::in);
+								if(file.is_open()){
+									file.close();
+									i=1;
+									do{
+										file.close();
+										aux_sstream<<i;
+										file.open(((string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+aux_sstream.str()+".tmx").c_str(),ios::in);
+										aux_sstream.clear();
+										i++;
+									}while(file.is_open());
+									file_name=(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+aux_sstream.str()+".tmx";
+								}
+								else
+									file_name=(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+".tmx";
+								bitext.GenerateBitext(file_name);
+								cout<<"\tThe bitext between "<<GetFileName(bitext.GetFirstWebFile().GetPath())<<" and "<<GetFileName(bitext.GetSecondWebFile().GetPath())<<" has been created."<<endl;
+								cout<<"\tEdit distance: "<<bitext.GetEditDistance()<<"%  Size difference:"<<bitext.GetSizeDistance()<<"%"<<endl<<endl;
+							}
+							if(results.size()==0)
+								cout<<"No correspondences were found between the files in the specified directory."<<endl;
+						}
+					}
+					else if(strcmp(argv[2],"-w")==0){
+						DownloadMod mod;
+						cout<<"Downloading from "<<argv[3]<<" (this will take some time)..."<<endl;
+						mod.SetDestPath(GlobalParams::GetDownloadPath());
+						mod.StartDownload(argv[3]);
+		
+						cout<<"Inicialising Bitextor's destination path..."<<endl; 
+						if(!ws.Initialize(GlobalParams::GetDownloadPath()+(string)argv[3]+"/"))
+							cerr<<"There were an error while trying to load the files in the selected directory."<<endl;
+						else{
+							mkdir((GlobalParams::GetDownloadPath()+(string)argv[3]+"/bitexts/").c_str(),0777);
+							cout<<"Comparing the files..."<<endl;
+							results=ws.GetMatchedFiles();
+							for(n_results=0;n_results<results.size();n_results++){
+								bitext=results[n_results];
+								file.open((GlobalParams::GetDownloadPath()+(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+".tmx").c_str(),ios::in);
+								if(file.is_open()){
+									file.close();
+									i=1;
+									do{
+										file.close();
+										aux_sstream<<i;
+										file.open((GlobalParams::GetDownloadPath()+(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+aux_sstream.str()+".tmx").c_str(),ios::in);
+										aux_sstream.clear();
+										i++;
+									}while(file.is_open());
+									file_name=GlobalParams::GetDownloadPath()+(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+aux_sstream.str()+".tmx";
+								}
+								else
+									file_name=GlobalParams::GetDownloadPath()+(string)argv[3]+"/bitexts/"+GetFileName(bitext.GetFirstWebFile().GetPath())+"_"+GetFileName(bitext.GetSecondWebFile().GetPath())+".tmx";
+								bitext.GenerateBitext(file_name);
+								cout<<"\tThe bitext between "<<GetFileName(bitext.GetFirstWebFile().GetPath())<<" and "<<GetFileName(bitext.GetSecondWebFile().GetPath())<<" has been created."<<endl;
+								cout<<"\tEdit distance: "<<bitext.GetEditDistance()<<"%  Size difference:"<<bitext.GetSizeDistance()<<"%"<<endl<<endl;
+							}
+							if(results.size()==0)
+								cout<<"No correspondences were found between the files in the specified directory."<<endl;
+						}
+					}
+				}
+			}
+			catch(char* e){
+				cout<<e<<endl;
+			}
+		}
+	}
+	if(show_howtouse)
+		cout<<"The correct way to call Bitextor is:"<<endl<<"\tbitextor config_file_path -w url"<<endl<<"\tbitextor config_file_path -d local_directory_with_html_files"<<endl<<endl<<"To see this use instructions, call: bitextor -h"<<endl;
+	return 0; 
+}
