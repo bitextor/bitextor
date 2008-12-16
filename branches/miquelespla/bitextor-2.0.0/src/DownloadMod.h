7344e0be45ff4ed5ceb2713cffe5b9413be9ec4d
espla <espla@96836ec2-454e-0410-a250-9ee83a685b91>
2008-12-16 11:44:49 +0000

    Compatible version for TagAligner 2.1

--- branches/miquelespla/bitextor-2.0.0/src/DownloadMod.h
+++ branches/miquelespla/bitextor-2.0.0/src/DownloadMod.h
@@ -27,14 +27,8 @@ using namespace std;
 class DownloadMod
 {
 	private:
-	
 	/**
-	 * Paràmetre que registra les extenssions dels fitxers objectius de la descàrrega.
-	 */
-	vector<string> accepted_extenssions;
-	
-	/**
-	 * Paràmetre que conté la grandària màxima (en bytes) del conjunt de fitxers descarregats. 
+	 * Paràmetre que conté la duració màxima (en segons) de la descàrrega de fitxers.
 	 */
 	long max_downloaded_size;
 	 
@@ -42,13 +36,6 @@ class DownloadMod
 	 * Directori de destí en què es guardaran els fitxers descarregats.
 	 */
 	string dest_path;
-	
-	private:
-	/**
-	 * Mètode que genera una cadena de caràcters formada per una llista d'extenssions separades per comes.
-	 * @return El mètode retorna una cadena composta per totes les extenssions contingudes en la llista d'extenssions admeses, separades per una coma. En cas què la llista estaga buida, retorna una cadena buida.
-	 */
-	const char* FormatAcceptedExtenssions();
 	
 	public:  
 	/**
@@ -65,26 +52,8 @@ class DownloadMod
 	 * Mètode que permet establir la mida màxima del conjunt d'elements descarregats.
 	 * @param mds Valor de la mida màxima de descàrrega.
 	 */
-	void SetMaxDownloadedSize(long mds);
+	void SetMaxDownloadedSize(long mdt);
 
-	/**
-	 * Mètode que permet afegir una nova extenssió a la llista d'extenssions permeses.
-	 * @param extenssion Paràmetre que conté l'extenssió que es dessitja afegir a la llista d'extenssions objectiu de la descàrrega de fitxers.
-	 */
-	void AddAcceptedExtenssions(string extenssion);
-
-	/**
-	 * Mètode que permet afegir noves extenssions a la llista d'extenssions permeses.
-	 * @param extenssion_list Mètode que permet afegir un conjunt d'extenssions a la llista d'extenssions objectiu de la descàrrega de fitxers.
-	 */
-	void AddAcceptedExtenssions(vector<string> extenssion_list);
-	
-	/**
-	 * Mètode que permet eliminar exensions de la llista d'extenssions acceptades.
-	 * @param extenssion_list Mètode que permet eliminar un conjunt d'extenssions de la llista d'extenssions objectiu de la descàrrega de fitxers.
-	 */
-	void RemoveAcceptedExtenssions(vector<string> extenssion_list);
-	
 	/**
 	 * Mètode que permet definir el directori on es realitzaran les descàrregues dels llocs web.
 	 * @param path Ruta que defineix el directori on es descarregaran els llocs web.
