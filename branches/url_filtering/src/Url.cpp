030ad307abff831d098c1a253399b73d95e49620
espla <espla@96836ec2-454e-0410-a250-9ee83a685b91>
2009-11-13 19:37:19 +0000

    Adding a new branch for url extraction from HTtrack log file

---
+++ branches/url_filtering/src/Url.cpp
@@ -0,0 +1,255 @@
+/*
+ * Autor: Miquel Esplà i Gomis [miquel.espla@ua.es]
+ * Any: 2009 
+ */
+
+#include <libtagaligner/ConfigReader.h>
+#include "Url.h"
+#include "GlobalParams.h"
+#include <cmath>
+#include <tre/regex.h>
+#include <fstream>
+
+Url::Url(wstring url){
+	unsigned int i;
+	bool variables=false, reading_var_name=true;
+	wstring str_tmp=L"", var_name=L"", var_val=L"";
+	
+	this->url=url;
+	#ifdef DEBUG_URL_INIT
+		wcout<<L"URL:"<<url<<endl;
+	#endif
+	for(i=0;i<url.length();i++){
+		if(variables){
+			if(reading_var_name){
+				if(url[i]==L'=')
+					reading_var_name=false;
+				else
+					var_name+=url[i];
+			}
+			else{
+				if(url[i]==L'&'){
+					this->variables[var_name]=var_val;
+					#ifdef DEBUG_URL_INIT
+						wcout<<L"\tVARIABLE: "<<var_name<<L"="<<var_val<<endl;
+					#endif
+					var_name=L"";
+					var_val=L"";
+					reading_var_name=true;
+				}
+				else
+					var_val+=url[i];
+			}
+		}
+		else{
+			if(url[i]==L'/'){
+				#ifdef DEBUG_URL_INIT
+					wcout<<L"\tDIRECTORY: "<<str_tmp<<endl;
+				#endif
+				this->directories.push_back(str_tmp);
+				str_tmp=L"";
+			}
+			else{
+				if(url[i]==L'?'){
+					variables=true;
+					this->filename=str_tmp;
+					#ifdef DEBUG_URL_INIT
+						wcout<<L"\tFILENAME: "<<str_tmp<<endl;
+					#endif
+					str_tmp=L"";
+				}
+				else{
+					str_tmp+=url[i];
+				}
+			}
+		}
+	}
+	if(variables){
+		this->variables[var_name]=var_val;
+		#ifdef DEBUG_URL_INIT
+			wcout<<L"\tVARIABLE: "<<var_name<<L"="<<var_val<<endl;
+		#endif
+	}
+	else
+		this->filename=str_tmp;
+}
+
+wstring Url::GetCompleteURL(){
+	return url;
+}
+	
+wstring Url::GetDirectoriy(unsigned int &index){
+	if(index>directories.size())
+		return L"";
+	else
+		return directories[index];
+}
+
+wstring Url::GetVariableValue(wstring &var_name){
+	map<wstring,wstring>::iterator it;
+	
+	it=variables.find(var_name);
+	if(it==variables.end())
+		return L"";
+	else
+		return it->second;
+}
+
+wstring Url::GetFilename(){
+	return filename;
+}
+
+unsigned int Url::GetNumberDirectories(){
+	return directories.size();
+}
+
+unsigned int Url::GetNumberVariables(){
+	return variables.size();
+}
+
+bool Url::VariableExists(wstring &var_name){
+	return(variables.find(var_name)!=variables.end());
+}
+
+double Url::ComparisonPoints(Url* url){
+	unsigned int i, max_len;
+	unsigned int not_coincident_dirs=0, not_coincident_vars=0, not_coincident_names=0;
+	map<wstring,wstring>::iterator it, it_fin;
+	
+	//Calculating differences between directories
+	if(directories.size()>url->directories.size())
+		max_len=url->directories.size();
+	else
+		max_len=directories.size();
+
+	for(i=0;i<directories.size();i++){
+		if(directories[i]!=url->directories[i])
+			not_coincident_dirs++;
+	}
+	not_coincident_dirs+=abs((double)(directories.size()-url->directories.size()));
+	
+	//Calculating differences between variables
+	if(variables.size()>url->variables.size()){
+		it=url->variables.begin();
+		it_fin=url->variables.end();
+	}
+	else{
+		it=variables.begin();
+		it_fin=variables.end();
+	}
+		
+	while(it!=it_fin){
+		if(it->second!=url->variables[it->first])
+			not_coincident_vars++;
+		it++;
+	}
+	not_coincident_vars+=abs((double)(variables.size()-url->variables.size()));
+	
+	//Calculating if filename is different
+	if(filename!=url->filename)
+		not_coincident_names++;
+	
+	#ifdef DEBUG_URL_CMP
+		wcout<<L"DIFFERENCES IN "<<url<<L": "<<not_coincident_vars+not_coincident_dirs<<endl;
+	#endif
+	
+	if((not_coincident_vars+not_coincident_dirs+not_coincident_names)==1)
+		return -0.1;
+	else if((not_coincident_vars+not_coincident_dirs)>1)
+		return 0.2;
+	else
+		return 0;
+}
+
+bool Url::ProcessHTtrackLogFile(string &file_path){
+	bool exit;
+	int status;
+	FILE* fin;
+	wstring aux=L"";
+	wint_t aux_car;
+	unsigned int found, i;
+	regex_t re1, re2, re3, re4;
+	regmatch_t matches1[1], matches2[1];
+	wstring tmp_str, url_and_filename;
+	wofstream fout;
+	
+	
+	if (regwcomp(&re1, L"[^\t\n ]+\t+[^\t\n ]+\t+[^\t\n ]+\t+[ae]", REG_EXTENDED) != 0)
+		return false;
+	if (regwcomp(&re2, L"^[a-z]+", REG_EXTENDED) != 0)
+		return false;
+	if (regwcomp(&re3, L" [^\t\n ]+\t+[^\t\n ]+\t+[^\t\n ]+\t+[^\t\n ]", REG_EXTENDED) != 0)
+		return false;
+	if (regwcomp(&re4, L"[^\t\n ]+\t+[^\t\n ]+", REG_EXTENDED) != 0)
+		return false;
+
+	fin=fopen(file_path.c_str(),"r");
+	fout.open("/home/miquel/URLList.xml");
+	if(fin){
+		if(fout.is_open()){
+			fout<<L"<?xml version=\"1.0\" encoding='UTF-8'?>"<<endl<<L"\t<urlsAndFiles>"<<endl;
+
+			aux_car=getwc(fin);
+			while(aux_car!=WEOF){
+				if(aux_car==L'\n'){
+					/*found=aux.find(L"<!-- Mirrored from ");
+					if (found<aux.length()){
+						found=aux.find_first_of(L' ',20);
+						url=new Url(aux.substr(19,found-19));
+						aux_car=WEOF;
+					}
+					else
+						aux_car=getwc(fin);*/
+					status = regwexec(&re1, aux.c_str(), (size_t) 1, matches1, 0);
+					if(status==0){
+						status = regwexec(&re2, aux.substr(matches1[0].rm_eo-1).c_str(), (size_t) 1, matches2, 0);
+						if(status==0 && aux.substr(matches1[0].rm_eo-1,matches2[0].rm_eo)==L"added"){
+							tmp_str=aux.substr(matches1[0].rm_eo-1+matches2[0].rm_eo);
+							status = regwexec(&re3, tmp_str.c_str(), (size_t) 1, matches1, 0);
+							if(status==0){
+								status = regwexec(&re4, tmp_str.substr(matches1[0].rm_eo).c_str(), (size_t) 1, matches2, 0);
+								if(status==0){
+									url_and_filename=tmp_str.substr(matches1[0].rm_eo-1,matches2[0].rm_eo);
+									fout<<L"\t\t<file><url>";
+									for(i=0;i<url_and_filename.length();i++){
+										if(url_and_filename[i]!='\t'){
+											if(url_and_filename[i]!='&')
+												fout<<url_and_filename[i];
+											else
+												fout<<L"&amp;";
+										}
+										else
+											fout<<L"</url><filename>";
+									}
+									fout<<L"</filename></file>"<<endl;
+								}
+							}
+						}
+					}
+					aux=L"";
+				}
+				else
+					aux+=(wchar_t)aux_car;
+				aux_car=getwc(fin);
+			}
+			fout<<L"\t</urlsAndFiles>"<<endl;
+			fout.close();
+			exit=true;
+		}
+		else{
+			exit=false;
+			wcout<<"NO S'HA POGUT OBRIR EL FITXER D'EIXIDA"<<endl;
+		}
+		fclose(fin);
+	}
+	else{
+		exit=false;
+		wcout<<"NO S'HA POGUT OBRIR EL FITXER D'ENTRADA"<<endl;
+	}
+		
+    regfree(&re1);
+    regfree(&re2);
+    regfree(&re3);
+    regfree(&re4);
+	return exit;
+}
---
+++ branches/url_filtering/src/WebFile.h
@@ -0,0 +1,157 @@
+#ifndef WEBFILE_H_
+#define WEBFILE_H_
+
+#include "GlobalParams.h"
+#include "FilePreprocess.h"
+#include "Url.h"
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+extern "C"{
+#include <textcat.h>
+}
+using namespace std;
+
+/**
+ * @class WebFile
+ * @brief Classe que conté els elements d'un fitxer web.
+ * 
+ * Classe que representa els elements continguts per un lloc web.
+ * Conté informació sobre els fitxers que pengen de l'arbre de
+ * directoris diferenciats per nivells, de forma que es pot ordenar,
+ * fàcilment, la informació dels fitxers descarregats pel DownloadMod.
+ * 
+ * @author Miquel Esplà i Gomis. 
+ */
+class WebFile
+{
+private:
+	/**
+	 * Adreça on es troba el fitxer.
+	 */
+	string path;
+	
+	/**
+	 * Lengua en què està escrit el text contingut pel fitxer.
+	 */
+	wstring lang;
+	
+	/**
+	 * Tipus de dades contingudes al fitxer.
+	 */
+	string file_type;
+	
+	/**
+	 * Objecte que conté el fitxer HTML dividit en etiquetes i blocs de text.
+	 */
+	vector<int> file;
+
+	/**
+	 * Indicador que assenyala si la classe ha estat inicialitzada correctament (si està a <code>true</code>).
+	 */
+	bool initialized;
+	
+	/**
+	 * Vector de nombres enters trobats al text
+	 */
+	vector<int> numbers_vec;
+
+	/**
+	 * Mida total del text (en caràcters) continguda al document.
+	 */
+	unsigned int text_size;
+	
+	Url *url;
+
+	/**
+	 * Mètode que indica si un caràcter és alfabètic o no.
+	 * @param car Caràcter a analitzar
+	 * @return Retorna <code>true</code> si el caràcter és alfabètic i <code>false</code> en cas contrari.
+	 */
+	bool IsAlphabetic(const wchar_t& car);
+	
+	/**
+	 * Method wich obtains the url of the webfile from a comment in the HTML code (if it has been downloaded by using
+	 * HTTrack)
+	 */
+	//void ObtainURL();
+
+public:
+
+	/**
+	 * Constructor per defecte de la classe.
+	 */
+	WebFile();
+	
+	/**
+	 * Destructor de la classe.
+	 */
+	~WebFile();
+	
+	/**
+	 * Inicialitzador de la classe amb tots els paràmetres a partir de la ruta del fitxer.
+	 * El mètode fa servir l'analitzador lèxic fet en Flex per a crear un vector d'etique-
+	 * tes i guardar el text net (sense etiquetes) en una variable, a partir de la qual es
+	 * detectarà l'idioma en què està escrit el text.
+	 * @param path Ruta del fitxer a què fa referència la classe.
+	 * @throw char* El mètode llança una excepció si no s'ha especificat el fitxer de configuració de TextCat.
+	 */
+	bool Initialize(const string &path);
+	
+	/**
+	 * Mètode que permet obtenir el paràmetre sobre l'idioma del fitxer.
+	 * @throw char* El mètode llança una excepció si no s'ha inicialitzat correctament l'objecte.
+	 * @return Retorna el codi d'idioma en què està escrit el text del fitxer.
+	 */
+	wstring GetLang();
+	
+	/**
+	 * Mètode que permet obtenir el paràmetre sobre la ruta on es troba el fitxer en el sistema de directoris.
+	 * @throw char* El mètode llança una excepció si no s'ha inicialitzat correctament l'objecte.
+	 * @return Retorna la ruta on es troba el fitxer en el sistema de directoris.
+	 */
+	string GetPath();
+	
+	/**
+	 * Mètode que permet obtenir el valor del paràmetre sobre el tipus de contingut (extensió) del fitxer.
+	 * @throw char* El mètode llança una excepció si no s'ha inicialitzat correctament l'objecte.
+	 * @return Retorna l'extensió del fitxer.
+	 */
+	string GetFileType();
+	
+	/**
+	 * Mètode que indica si el fitxer està inicialitzat correctament.
+	 * @return Retorna <code>true</code> si el fitxer es torba correctament inicialitzat i <code>false</code> en cas contrari.
+	 */
+	bool IsInitialized();
+
+	/**
+	 * Mètode que retorna un punter al vector d'enters continguts al text.
+	 * @return Retorna un punter al vector d'enters continguts al text.
+	 */
+	vector<int>* GetNumbersVector();
+
+	/**
+	 * Mètode que retorna la mida total en caràcters del text del document.
+	 * @return Retorna la mida total en caràcters del text del document.
+	 */
+	unsigned int GetTextSize();
+
+	/**
+	 * Mètode que retorna un punter a l'array d'etiquetes-blocs de text.
+	 * @return Retorna un punter a l'array d'etiquetes-blocs de text.
+	 */
+	vector<int>* GetTagArray();
+
+	/**
+	 * Mètode que carrega a l'array d'enters els números enters continguts al text.
+	 * @param text Text del qual s'hi volen extreure els números.
+	 */
+	void GetNonAplha(wstring text);
+
+	Url* GetURL();
+};
+
+#endif /*WEBFILE_H_*/
