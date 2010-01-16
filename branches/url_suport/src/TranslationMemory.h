910548468d86c01f4691d9fee16deca9bb1c6559
espla <espla@96836ec2-454e-0410-a250-9ee83a685b91>
2010-01-16 12:52:58 +0000

    Translating doxygen comments

--- branches/url_suport/src/TranslationMemory.h
+++ branches/url_suport/src/TranslationMemory.h
@@ -8,68 +8,72 @@
 
 /**
  * @class TranslationMemory
- * @brief Classe que conté els elements necessaris per generar els fitxers de traducció.
+ * @brief This class contains the necessary elements to generaty translation memories (TM) from the foud bitexts in a
+ * website.
  * 
- * Bàsicament, la classe enera el contingut i gestiona els fitxers,
- * de forma que escolleix quin contingut va en quin fitxer.
+ * This class contains the necessary elements to generaty translation memories (TM) from the foud bitexts in a  website.
+ * It manages the TMXs files which contains the TMs. This class can work in two ways: writting each TM in a different file
+ * or writting all the translation unit (TU) in an only file for a given language pair. To work in the second way, this
+ * class needs to manage as many files as language pairs exist in the website and it will only close these files when the
+ * application has finished the complete process.
  * 
  * @author Miquel Esplà i Gomis. 
  */
 class TranslationMemory
 {
 	private:
-		/** Ruta en què es generaran les memòries de traducció. */
+		/** Path where the TMXs files will be created and saved. */
 		static string dest_path;
 		
 		/** 
-		 * Array emprat quan s'executa l'aplicació generant només una memòria
-		 * de traducció per parell de llengües. Gestiona el conjunt de fitxers
-		 * per anar desant els nous TU's.
+		 * If the application is runed to create an only TM for each pair of languages, the corresponding TMX file for
+		 * each TM is saved in this structure. In this way, the left element in the Map corresponds to the language
+		 * pair identifier. The rigth one corresponds to a pair of values: the TMX file where the TU are been saved and
+		 * the last TU identificator (each TU is identified with an integer in the TMX file). Saving the TU identificator
+		 * the system can avoid the TU identification numbers repetition.
 		 */
 		static map< wstring, pair< FILE*,int >* > uniq_files;
 	
 	public:
 	
 		/**
-		 * Mètode que permet assignar la ruta de destí de les memòries de traducció.
-		 * @param path Nova ruta a assignar.
+		 * This method sets the destination path where the TMs will be crated and saved.
+		 * @param path Assigned path.
 		 */
 		static void SetDestPath(const string &path);
 
 		/**
-		 * Mètode que genera una memòria de traducció a partir de dues pàgines web
-		 * i les desa en un sol fitxer, per al parell de llengües, on s'hi desaran
-		 * la resta de TU's dels altres fitxers web analitzats.
-		 * @param wf1 Un dels fitxers web del qual s'obtindrà la memòria de traducció.
-		 * @param wf2 L'altre fitxer web des del qual s'obtindrà la memòria de traducció.
-		 * @param data Informació resultant de la comparació dels fitxers.
-		 * @return Retorna <code>true</code> si s'ha generat la memòria de traducció i <code>false</code> en cas contrari.
+		 * This method generates a TM from a pair of web files. When it has generated all the TU, it saves it in the
+		 * file corresponding with the language pair of the files.
+		 * @param wf1 First web file.
+		 * @param wf2 Second web file.
+		 * @param data Information generated during the web files comparison (see BitextData class documentation).
+		 * @return Returns <code>true</code> if the TM has been generated and <code>false</code> if not.
 		 */
 		static bool WriteInSameFile(WebFile* wf1, WebFile* wf2, BitextData* data);
 
 		/**
-		 * Mètode que genera una memòria de traducció a partir de dues pàgines web
-		 * i la desa en un fitxer pròpi per al parell de fitxers comparats en qüestió.
-		 * @param wf1 Un dels fitxers web del qual s'obtindrà la memòria de traducció.
-		 * @param wf2 L'altre fitxer web des del qual s'obtindrà la memòria de traducció.
-		 * @param data Informació resultant de la comparació dels fitxers.
-		 * @return Retorna <code>true</code> si s'ha generat la memòria de traducció i <code>false</code> en cas contrari.
+		 * This method generates a TM from a pair of web files. When it has generated all the TU, it saves it in an only
+		 * file for this pair of files.
+		 * @param wf1 First web file.
+		 * @param wf2 Second web file.
+		 * @param data Information generated during the web files comparison (see BitextData class documentation).
+		 * @return Returns <code>true</code> if the TM has been generated and <code>false</code> if not.
 		 */
 		static bool WriteInDifferentFile(WebFile* wf1, WebFile* wf2, BitextData* data);
 
 		/**
-		 * Mètode que genera una memòria de traducció a partir de dues pàgines web. Depenent
-		 * del mode en què s'estiga executant Bitextor, crida el mètode WriteInSameFile o
-		 * WriteInDifferentFile.
-		 * @param wf1 Un dels fitxers web del qual s'obtindrà la memòria de traducció.
-		 * @param wf2 L'altre fitxer web des del qual s'obtindrà la memòria de traducció.
-		 * @param data Informació resultant de la comparació dels fitxers.
-		 * @return Retorna <code>true</code> si s'ha generat la memòria de traducció i <code>false</code> en cas contrari.
+		 * This method generates a TM from a pair of web files. This is the method which chooses if the TM must be saved
+		 * in an individual TMX file for the pair of files or in a general TMX file for the pair of languages.
+		 * @param wf1 First web file.
+		 * @param wf2 Second web file.
+		 * @param data Information generated during the web files comparison (see BitextData class documentation).
+		 * @return Returns <code>true</code> if the TM has been generated and <code>false</code> if not.
 		 */
 		static bool WriteTM(WebFile* wf1, WebFile* wf2, BitextData* data);
 		
 		/**
-		 * Mètode que tanca tots els fitxers oberts i elimina la memòria ocupada pels objectes instanciats
+		 * This method closes all the opened files and frees the dynamic memory used by the class during the TM generation.
 		 * per la classe.
 		 */
 		static void Reset();
