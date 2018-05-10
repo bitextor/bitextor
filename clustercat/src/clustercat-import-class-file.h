27ba37b81ede03d4d19cfa6eacf5dd5d17928c6b
Lesbinary <leopoldoplasempere@gmail.com>
2018-05-10 11:03:06 +0200

    Cleaned some code for code release of Paracrawl

--- clustercat/src/clustercat-import-class-file.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_CLUSTERCAT_IMPORT_CLASS_FILE_HEADER
-#define INCLUDE_CLUSTERCAT_IMPORT_CLASS_FILE_HEADER
-
-#include "clustercat.h" // wclass_t
-
-void import_class_file(struct_map_word **word_map, wclass_t word2class[restrict], const char * restrict class_file_name, const wclass_t num_classes);
-
-#endif // INCLUDE_HEADER
