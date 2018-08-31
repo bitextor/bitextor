dcd711878e6c1bc8bed3c13c5e6ef13b7b23c759
Leopoldo Pla <leopoldoplasempere@gmail.com>
2018-08-31 12:43:37 +0000

    More fixes for inline tags and non-inline (block) tags tokens, more documentation

--- bitextor-deferred-reconstructor.py
+++ bitextor-deferred-reconstructor.py
@@ -14,23 +14,24 @@ def get_sentence(standoff,document):
     Retrieves the original content from a XHTML/XML html5lib parsed document pointed by a standoff annotation
     """
     reconstructedsentence = []
-    for wordstandoff in standoff.split(';'):
-        wordstandoffsegments = wordstandoff.split('+')
+    for wordstandoff in standoff.split(';'):    #Split by standoff separator ';'
+        wordstandoffsegments = wordstandoff.split('+')  #Split by '+' in case of glued annotations
         for wordstandoffseg in wordstandoffsegments:
-            wordstandoffparts = wordstandoffseg.split(':')
+            wordstandoffparts = wordstandoffseg.split(':')  #Obtain path and length range/limits
             wordstandofflimits = wordstandoffparts[1].split('-')
-            element = document.find(wordstandoffparts[0])
+            element = document.find(wordstandoffparts[0])   #Get lxml element from path
             if element.text is None:
                 element.text = ""
-            if int(wordstandofflimits[1]) < len(element.text):
-                if wordstandoffseg == wordstandoffsegments[0]:
+            if int(wordstandofflimits[1]) < len(element.text):  #Search word in element.text or in children tail depending on the limits
+                if wordstandoffseg == wordstandoffsegments[0]:  #Create new annotation or add to previous word reconstruction if glued annotation
                     reconstructedsentence.append(element.text[int(wordstandofflimits[0]):int(wordstandofflimits[1])+1])
                 else:
                     reconstructedsentence[-1] = reconstructedsentence[-1] + element.text[int(wordstandofflimits[0]):int(wordstandofflimits[1])+1]
             else:
                 tail = element.text
                 for child in element:
-                    tail = tail + child.tail
+                    if child.tail is not None:
+                        tail = tail + child.tail
                     if len(tail) > int(wordstandofflimits[1]):
                         if wordstandoffseg == wordstandoffsegments[0]:
                             reconstructedsentence.append(tail[int(wordstandofflimits[0]):int(wordstandofflimits[1])+1])
@@ -41,21 +42,24 @@ def get_sentence(standoff,document):
 
 
 
-
+#Argument input: path of original Bitextor formatted crawl file
 document_standoff = dict()
 with open(sys.argv[1],'r') as reader:
     for line in reader:
         fields=line.split('\t')
         fields = list(map(str.strip, fields)) #Strip all elements
-        fields.append(str(time.time())) #Timestamp
-        fields.append(hashlib.md5(base64.b64decode(fields[0])).hexdigest()) #MD5 document checksum
         #We use lxml treebuilder because of getelementpath function and iteration through elements
-        document_standoff[fields[1]] = html5lib.parse(base64.b64decode(fields[0]),treebuilder="lxml",namespaceHTMLElements=False)
-    
+        document_standoff[fields[1]] = html5lib.parse(base64.b64decode(fields[0]),treebuilder="lxml",namespaceHTMLElements=False) #Store url:html5lib_tree for easy path search
+
+#Input: Bitextor DOCALG file (deferred):
+#url1 url2 deferred_clean_text1_in_base64 deferred_clean_text2_in_base64
+
+#Output: Bitextor DOCALG file reconstructed:
+#url1 url2 clean_text1_in_base64 clean_text2_in_base64
 for line in sys.stdin:
     fields = line.split('\t')
     newfields = [fields[0],fields[1]]
-    for annotation,url in {fields[2]:fields[0],fields[3]:fields[1]}.items():
+    for annotation,url in {fields[2]:fields[0],fields[3]:fields[1]}.items(): #SL and TL annotations with URLs from input DOCALG file format: https://github.com/bitextor/bitextor/wiki/Intermediate-formats-used-in-Bitextor#docalg
         newfields.append(get_sentence(annotation,document_standoff[url]))
     print("\t".join(newfields))
 
--- bitextor-deferred-sentences.py
+++ bitextor-deferred-sentences.py
@@ -8,21 +8,25 @@ import html5lib
 from lxml import etree
 import re
 
+
+#Argument input: deferred Bitextor crawl file with plain text and its standoff annotations (output of deferred document script)
 documentStandoff = dict()
 with open(sys.argv[1],'r') as reader:
     for line in reader:
         fields = line.split('\t')
         fields = list(map(str.strip, fields))
-        document = html5lib.parse(base64.b64decode(fields[0]),treebuilder="lxml",namespaceHTMLElements=False)
-        documentStandoff[fields[1]]=(document,fields[4].split(';'))
+        documenttext = base64.b64decode(fields[0]).decode('utf8')
+        documentStandoff[fields[1]]=(documenttext,fields[4].split(';'))
 
+#Input: Bitextor DOCALG file:
+#url1 url2 clean_text1_in_base64 clean_text2_in_base64
 for line in sys.stdin:
     fields = line.split('\t')
     fields = list(map(str.strip, fields)) #Strip all elements
 
-    shortpathSL=WagnerFischer(fields[2].split(' '),[x for x in etree.tostring(documentStandoff[fields[0]][0], encoding='unicode', method="text").replace('\n','').replace('\t','').replace('\xa0','').split(' ') if x != '']).optimum_alignments() #Calculate a short distance path using Wagner-Fischer algorithm for source
-    shortpathTL=WagnerFischer(fields[3].split(' '),[x for x in etree.tostring(documentStandoff[fields[1]][0], encoding='unicode', method="text").replace('\n','').replace('\t','').replace('\xa0','').split(' ') if x != '']).optimum_alignments() #and target sentences
-   
+    shortpathSL=WagnerFischer(fields[2].split(' '),[x for x in documentStandoff[fields[0]][0].replace('\n',' ').replace('\t',' ').replace('\xa0',' ').split(' ') if x != '']).optimum_alignments() #Calculate a short distance path using Wagner-Fischer algorithm for source
+    shortpathTL=WagnerFischer(fields[3].split(' '),[x for x in documentStandoff[fields[1]][0].replace('\n',' ').replace('\t',' ').replace('\xa0',' ').split(' ') if x != '']).optimum_alignments() #and target sentences
+    
     position=0
     standoffSL=[]
     for op in shortpathSL: #Obtain the standoff annotation of each sentence word from the full annotated document they come from, counting non-inserted words
@@ -33,7 +37,7 @@ for line in sys.stdin:
     
     position=0
     standoffTL=[]
-    for op in shortpathTL:
+    for op in shortpathTL: #Same for TL
         if op != "I":
             standoffTL.append(documentStandoff[fields[1]][1][position])
         position = position + 1
