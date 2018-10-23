b21b1151cb2a92e5b617fa9cfdc9f9e3c742f332
Hieu Hoang <hieuhoang@gmail.com>
2018-10-23 12:29:25 +0200

    post-install script

---
+++ slurm/install.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install cmake g++ automake pkg-config openjdk-8-jdk python3 python3-pip python3-magic libbz2-dev liblzma-dev zlib1g-dev libboost-all-dev maven nfs-kernel-server nfs-common parallel -y
+
+pip3 install --upgrade python-Levenshtein tensorflow keras iso-639 langid nltk regex h5py warc3-wet
+
+
