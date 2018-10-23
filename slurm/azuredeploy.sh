ef9924ebe79fafc80b63c15bad4a6917afea60f8
Leo <leopoldoplasempere@gmail.com>
2018-10-23 12:15:23 +0200

    Update azuredeploy.sh

--- slurm/azuredeploy.sh
+++ slurm/azuredeploy.sh
@@ -48,7 +48,7 @@ fi
 sudo apt-get install sshpass -y >> /tmp/azuredeploy.log.$$ 2>&1
 
 # Bitextor installation
-sudo apt-get update
+sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
 sudo apt-get install cmake -y >> /tmp/azuredeploy.log.$$ 2>&1
 sudo apt-get install g++ -y >> /tmp/azuredeploy.log.$$ 2>&1
 sudo apt-get install automake -y >> /tmp/azuredeploy.log.$$ 2>&1
@@ -61,7 +61,7 @@ sudo apt-get install maven -y >> /tmp/azuredeploy.log.$$ 2>&1
 sudo apt-get install nfs-kernel-server nfs-common -y >> /tmp/azuredeploy.log.$$ 2>&1
 
 sudo pip3 install --upgrade python-Levenshtein tensorflow keras iso-639 langid nltk regex h5py warc3-wet >> /tmp/azuredeploy.log.$$ 2>&1
-sudo -u $ADMIN_USERNAME sh -c "mkdir -p ~/workspace/software; git clone --recurse-submodules https://github.com/bitextor/bitextor.git ~/workspace/software/bitextor; cd ~/workspace/software/bitextor; ./autogen.sh --prefix=~/workspace/software/bitextor && make && make install"
+sudo -u $ADMIN_USERNAME sh -c "mkdir -p ~/workspace/software; git clone --recurse-submodules https://github.com/bitextor/bitextor.git ~/workspace/software/bitextor; cd ~/workspace/software/bitextor; ./autogen.sh --prefix=~/workspace/software/bitextor && make && make install" >> /tmp/azuredeploy.log.$$ 2>&1
 
 # Loop through all worker nodes, update hosts file and copy ssh public key to it
 # The script make the assumption that the node is called %WORKER+<index> and have
@@ -81,7 +81,6 @@ done
 ###################################
 
 # Install the package
-sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
 sudo chmod g-w /var/log >> /tmp/azuredeploy.log.$$ 2>&1 # Must do this before munge will generate key
 sudo apt-get install slurm-llnl -y >> /tmp/azuredeploy.log.$$ 2>&1
 
