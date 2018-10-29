b455ca03e4b07e0407c5dea2af15e7e9483ad2c6
mespla <mespla@dlsi.ua.es>
2018-10-29 09:56:44 +0100

    Adding bitextor-warc2tt

---
+++ bitextor-warc2tt.py
@@ -0,0 +1,10 @@
+#!__ENV__ __PYTHON__
+
+import warc
+import base64
+import sys
+
+f = warc.WARCFile(fileobj=sys.stdin.buffer)
+for record in f:
+    print(base64.b64encode(record.payload.read()).decode('utf8').replace("\t"," ")+"\t"+record.url+"\t"+record.date)
+
