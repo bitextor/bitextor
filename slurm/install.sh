cc54219e7540a953800a92c3d4b2165267b8485a
Hieu Hoang <hieuhoang@gmail.com>
2018-10-30 16:04:27 +0000

    tab to spaces

--- slurm/install.sh
+++ slurm/install.sh
@@ -11,55 +11,55 @@ echo "REGION $REGION"
 echo "vmssnames $vmssnames"
 
 if ! [ $SUDO_USER ]; then
-	echo "must run as sudo. Exiting"
-	exit
+    echo "must run as sudo. Exiting"
+    exit
 fi
 
 installdependencies(){
-        sudo apt-get update
-        sudo apt-get install -y g++ automake pkg-config openjdk-8-jdk python3 python3-pip python3-magic libbz2-dev liblzma-dev zlib1g-dev libboost-all-dev maven nfs-kernel-server nfs-common parallel sshpass emacs munge slurm-wlm ubuntu-drivers-common libicu-dev curl
+    sudo apt-get update
+    sudo apt-get install -y g++ automake pkg-config openjdk-8-jdk python3 python3-pip python3-magic libbz2-dev liblzma-dev zlib1g-dev libboost-all-dev maven nfs-kernel-server nfs-common parallel sshpass emacs munge slurm-wlm ubuntu-drivers-common libicu-dev curl
 
-	wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
-   	tar xvf cmake-3.12.3.tar.gz 
-   	cd cmake-3.12.3/
-   	./bootstrap 
-   	make -j4
-   	sudo make install
-	cd ..
-	rm -rf cmake-3.12.3.tar.gz cmake-3.12.3
+    wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
+    tar xvf cmake-3.12.3.tar.gz 
+    cd cmake-3.12.3/
+    ./bootstrap 
+    make -j4
+    sudo make install
+    cd ..
+    rm -rf cmake-3.12.3.tar.gz cmake-3.12.3
 
-        CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-        wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
-        sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-        sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
-        rm -f /tmp/${CUDA_REPO_PKG}
-        sudo apt-get update
-        sudo apt-get install -y cuda
+    CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
+    sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
+    rm -f /tmp/${CUDA_REPO_PKG}
+    sudo apt-get update
+    sudo apt-get install -y cuda
 
-	AZ_REPO=$(lsb_release -cs)
-	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |     sudo tee /etc/apt/sources.list.d/azure-cli.list
-	curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-	sudo apt-get update
-	sudo apt-get install -y apt-transport-https azure-cli
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |     sudo tee /etc/apt/sources.list.d/azure-cli.list
+    curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https azure-cli
         
 
-        sudo echo "CUDA_ROOT=/usr/local/cuda" >> /etc/environment
-        sudo echo "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda/bin\"" >> /etc/environment
-        sudo echo "LD_LIBRARY_PATH=\"/usr/local/cuda/lib64\"" >> /etc/environment
-        sudo echo "LIBRARY_PATH=\"/usr/local/cuda/lib64\"" >> /etc/environment
-        PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda/bin"
-        CUDA_ROOT="/usr/local/cuda"
-        LD_LIBRARY_PATH="/usr/local/cuda/lib64"
-        LIBRARY_PATH="/usr/local/cuda/lib64"
+    sudo echo "CUDA_ROOT=/usr/local/cuda" >> /etc/environment
+    sudo echo "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda/bin\"" >> /etc/environment
+    sudo echo "LD_LIBRARY_PATH=\"/usr/local/cuda/lib64\"" >> /etc/environment
+    sudo echo "LIBRARY_PATH=\"/usr/local/cuda/lib64\"" >> /etc/environment
+    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda/bin"
+    CUDA_ROOT="/usr/local/cuda"
+    LD_LIBRARY_PATH="/usr/local/cuda/lib64"
+    LIBRARY_PATH="/usr/local/cuda/lib64"
         
-        sudo pip3 install --upgrade python-Levenshtein tensorflow keras iso-639 langid nltk regex h5py warc3-wet snakemake tld
-        python3 -c "import nltk; nltk.download('punkt')"
-	sudo rm /tmp/munge.key /tmp/slurm.conf /tmp/hosts
+    sudo pip3 install --upgrade python-Levenshtein tensorflow keras iso-639 langid nltk regex h5py warc3-wet snakemake tld
+    python3 -c "import nltk; nltk.download('punkt')"
+    sudo rm /tmp/munge.key /tmp/slurm.conf /tmp/hosts
 
 }
 
 if [ "$INSTALL" != "no" ]; then
-	installdependencies
+    installdependencies
 fi
 
 # master only
@@ -71,19 +71,19 @@ if ! [ -f /home/$SUDO_USER/.ssh/id_rsa ]; then
 fi
 
 for vmssinfo in $vmssnames; do
-	VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
-	VM_SKU=`echo $vmssinfo | cut -f 2 -d ':'`
-	VM_COUNT=`echo $vmssinfo | cut -f 3 -d ':'`
-	echo "VMSS_NAME=$VMSS_NAME VM_SKU=$VM_SKU VM_COUNT=$VM_COUNT"
-	
-	#Create the scaleset
-	az vmss create --resource-group $RESOURCE_GROUP --name $VMSS_NAME --image "Canonical:UbuntuServer:18.04-LTS:18.04.201810030" -l $REGION --vm-sku $VM_SKU --instance-count $VM_COUNT --admin-username $ADMIN_USERNAME
-	if [ "$INSTALL" != "no" ]; then
-		for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
-			print "installing worker $worker"
-			sudo -u $SUDO_USER ssh -o "StrictHostKeyChecking=no" $worker "$(typeset -f installdependencies); installdependencies" &
-		done
-	fi
+    VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
+    VM_SKU=`echo $vmssinfo | cut -f 2 -d ':'`
+    VM_COUNT=`echo $vmssinfo | cut -f 3 -d ':'`
+    echo "VMSS_NAME=$VMSS_NAME VM_SKU=$VM_SKU VM_COUNT=$VM_COUNT"
+    
+    #Create the scaleset
+    az vmss create --resource-group $RESOURCE_GROUP --name $VMSS_NAME --image "Canonical:UbuntuServer:18.04-LTS:18.04.201810030" -l $REGION --vm-sku $VM_SKU --instance-count $VM_COUNT --admin-username $ADMIN_USERNAME
+    if [ "$INSTALL" != "no" ]; then
+        for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
+            print "installing worker $worker"
+            sudo -u $SUDO_USER ssh -o "StrictHostKeyChecking=no" $worker "$(typeset -f installdependencies); installdependencies" &
+        done
+    fi
 done
 wait
 
@@ -120,21 +120,21 @@ echo "GresTypes=gpu" >> $SLURMCONF
 allworkernames="$MASTER_NAME"
 echo "NodeName=${MASTER_NAME} CPUs=1 State=UNKNOWN" >> $SLURMCONF
 for vmssinfo in $vmssnames; do
-	VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
-	CPUs=`echo $vmssinfo | cut -f 4 -d ':'`
-	gpuinfo=`echo $vmssinfo | cut -f 5- -d ':'`
-	echo "VMSS_NAME=$VMSS_NAME CPUs=$CPUs gpuinfo=$gpuinfo"
-	
-	echo "$LIST" | grep -q "$SOURCE";
-	if echo "$vmssinfo" | grep -q ":gpu:" ; then
-		workernames=`az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"' | head -c -1 | tr '\n' ','`
-		allworkernames="$allworkernames,$workernames"
-		echo "NodeName=${workernames} CPUs=$CPUs State=UNKNOWN Gres=$gpuinfo" >> $SLURMCONF
-	else
-		workernames=`az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"' | head -c -1 | tr '\n' ','`
-		allworkernames="$allworkernames,$workernames"
-		echo "NodeName=${workernames} CPUs=$CPUs State=UNKNOWN" >> $SLURMCONF
-	fi
+    VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
+    CPUs=`echo $vmssinfo | cut -f 4 -d ':'`
+    gpuinfo=`echo $vmssinfo | cut -f 5- -d ':'`
+    echo "VMSS_NAME=$VMSS_NAME CPUs=$CPUs gpuinfo=$gpuinfo"
+    
+    echo "$LIST" | grep -q "$SOURCE";
+    if echo "$vmssinfo" | grep -q ":gpu:" ; then
+        workernames=`az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"' | head -c -1 | tr '\n' ','`
+        allworkernames="$allworkernames,$workernames"
+        echo "NodeName=${workernames} CPUs=$CPUs State=UNKNOWN Gres=$gpuinfo" >> $SLURMCONF
+    else
+        workernames=`az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"' | head -c -1 | tr '\n' ','`
+        allworkernames="$allworkernames,$workernames"
+        echo "NodeName=${workernames} CPUs=$CPUs State=UNKNOWN" >> $SLURMCONF
+    fi
 done
 echo "PartitionName=debug Nodes=${allworkernames} Default=YES MaxTime=INFINITE State=UP" >> $SLURMCONF
 echo "DebugFlags=NO_CONF_HASH" >> $SLURMCONF
@@ -155,18 +155,18 @@ sudo chown $SUDO_USER $mungekey
 echo $MASTER_IP $MASTER_NAME >> /etc/hosts
 
 copykeys(){
-        worker=$1
-        SUDO_USER=$2
-        sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no $mungekey $SUDO_USER@$worker:/tmp/munge.key
-        sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no /etc/slurm-llnl/slurm.conf $SUDO_USER@$worker:/tmp/slurm.conf
-        sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no /etc/hosts $SUDO_USER@$worker:/tmp/hosts
+    worker=$1
+    SUDO_USER=$2
+    sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no $mungekey $SUDO_USER@$worker:/tmp/munge.key
+    sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no /etc/slurm-llnl/slurm.conf $SUDO_USER@$worker:/tmp/slurm.conf
+    sudo -u $SUDO_USER scp -o StrictHostKeyChecking=no /etc/hosts $SUDO_USER@$worker:/tmp/hosts
 }
 for vmssinfo in $vmssnames; do
-	VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
-	paste <(az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"') <(az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"') >> /etc/hosts 
-	for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
-		copykeys $worker $SUDO_USER &
-	done
+    VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
+    paste <(az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"') <(az vmss list-instances --resource-group $RESOURCE_GROUP --name $VMSS_NAME | grep 'computerName' | cut -f 2 -d ':' | cut -f 2 -d '"') >> /etc/hosts 
+    for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
+        copykeys $worker $SUDO_USER &
+    done
 done
 wait
 
@@ -174,9 +174,9 @@ wait
 
 sudo -u $SUDO_USER sh -c "mkdir ~/workspace"
 if grep -q "/home/$SUDO_USER/workspace \*(rw,sync,no_subtree_check)" /etc/exports ; then
-	:
+    :
 else
-	sudo echo "/home/$SUDO_USER/workspace *(rw,sync,no_subtree_check)" >> /etc/exports
+    sudo echo "/home/$SUDO_USER/workspace *(rw,sync,no_subtree_check)" >> /etc/exports
 fi
 sudo systemctl restart nfs-kernel-server
 
@@ -211,10 +211,10 @@ slurmworkersetup(){
     sudo mount $MASTER_IP:/home/$SUDO_USER/workspace /home/$SUDO_USER/workspace
 }
 for vmssinfo in $vmssnames; do
-	VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
-	for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
-		sudo -u $SUDO_USER ssh -o StrictHostKeyChecking=no $worker -o "StrictHostKeyChecking no" "$(typeset -f slurmworkersetup); slurmworkersetup $SUDO_USER $MASTER_IP" &
-	done
+    VMSS_NAME=`echo $vmssinfo | cut -f 1 -d ':'`
+    for worker in `az vmss nic list --resource-group $RESOURCE_GROUP --vmss-name $VMSS_NAME | grep 'privateIpAddress"' | cut -f 2 -d ':' | cut -f 2 -d '"'`; do
+        sudo -u $SUDO_USER ssh -o StrictHostKeyChecking=no $worker -o "StrictHostKeyChecking no" "$(typeset -f slurmworkersetup); slurmworkersetup $SUDO_USER $MASTER_IP" &
+    done
 done
 wait
 
