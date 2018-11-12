22db0356e0a6c111060182eaba62a49eec98bc8b
Hieu Hoang <hieuhoang@gmail.com>
2018-11-12 17:47:32 +0000

    get ready to use amun. Call Marek's doc aligner - marek

--- snakemake/translate.sh
+++ snakemake/translate.sh
@@ -7,11 +7,14 @@ LANG2=$2
 
 MOSES=~/permanent/software/mosesdecoder
 MARIAN=~/permanent/software/marian-dev
+AMUN=~/permanent/software/amun
+
 SUBWORD_NMT=~/permanent/software/subword-nmt
 
 TC_MODEL=~/permanent/en-fr/nmt-dir/model/truecaser/truecase-model.en
 BPE_MODEL=~/permanent/en-fr/nmt-dir/model/vocab.enfr
 MARIAN_MODEL=~/permanent/data/fr-en/model/marian/model.npz.decoder.yml
+#AMUN_MODEL=~/permanent/data/fr-en/model/marian/model.npz.amun.yml
 
 #tee in |
 LC_ALL=C $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG1 |
@@ -19,6 +22,7 @@ LC_ALL=C $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL |
 $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL |
 #tee in.2 |
 $MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length-crop --max-length 50 |
+#$AMUN/build/amun -c $AMUN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length 50 --cpu-threads 4 |
 #tee out |
 sed -r 's/(@@ )|(@@ ?$)//g' |
 $MOSES/scripts/recaser/detruecase.perl |
