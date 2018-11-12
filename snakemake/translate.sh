229e7ae8fdde1b1831c444b6aba18bb23b114dda
Hieu Hoang <hieuhoang@gmail.com>
2018-11-12 16:51:12 +0000

    debug

--- snakemake/translate.sh
+++ snakemake/translate.sh
@@ -9,12 +9,13 @@ TC_MODEL=~/permanent/en-fr/nmt-dir/model/truecaser/truecase-model.en
 BPE_MODEL=~/permanent/en-fr/nmt-dir/model/vocab.enfr
 MARIAN_MODEL=~/permanent/data/fr-en/model/marian/model.npz.decoder.yml
 
-tee in |
+ #tee in |
  LC_ALL=C $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG |
  LC_ALL=C $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL |
- $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL | tee in.2 |
- $MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length-crop --max-length 50 |
- tee out
+ $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL |
+ #tee in.2 |
+ $MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length-crop --max-length 50
+ #tee out
 
 
 
