ffdcce8948fad55e4580a29e00bf8b8a12ed7a31
Hieu Hoang <hieuhoang@gmail.com>
2018-11-12 17:09:27 +0000

    post-process

--- snakemake/translate.sh
+++ snakemake/translate.sh
@@ -1,21 +1,26 @@
 
 
-MOSES=/home/hieu/workspace/github/mosesdecoder
-MARIAN=/home/hieu/workspace/github/marian-dev
-SUBWORD_NMT=/home/hieu/workspace/github/subword-nmt
+MOSES=~/permanent/software/mosesdecoder
+MARIAN=~/permanent/software/marian-dev
+SUBWORD_NMT=~/permanent/software/subword-nmt
 
-LANG=en
+LANG1=en
+LANG2=fr
 TC_MODEL=~/permanent/en-fr/nmt-dir/model/truecaser/truecase-model.en
 BPE_MODEL=~/permanent/en-fr/nmt-dir/model/vocab.enfr
 MARIAN_MODEL=~/permanent/data/fr-en/model/marian/model.npz.decoder.yml
 
- #tee in |
- LC_ALL=C $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG |
- LC_ALL=C $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL |
- $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL |
- #tee in.2 |
- $MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length-crop --max-length 50
- #tee out
+#tee in |
+LC_ALL=C $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG1 |
+LC_ALL=C $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL |
+$SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL |
+#tee in.2 |
+$MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 --max-length-crop --max-length 50 |
+#tee out |
+sed -r 's/(@@ )|(@@ ?$)//g' |
+$MOSES/scripts/recaser/detruecase.perl |
+$MOSES/scripts/tokenizer/detokenizer.perl -l $LANG2 #|
+#tee out.2
 
 
 
