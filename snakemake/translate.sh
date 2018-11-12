9cb079d25c045efaa8551accdef0f6fb9efe99d3
Hieu Hoang <hieuhoang@gmail.com>
2018-11-12 15:56:08 +0000

    hardcoded translate.sh

--- snakemake/translate.sh
+++ snakemake/translate.sh
@@ -7,7 +7,14 @@ SUBWORD_NMT=/home/hieu/workspace/github/subword-nmt
 LANG=en
 TC_MODEL=~/permanent/en-fr/nmt-dir/model/truecaser/truecase-model.en
 BPE_MODEL=~/permanent/en-fr/nmt-dir/model/vocab.enfr
-MARIAN_MODEL=~/permanent/data/fr-en/marian/model.npz.decoder.yml
+MARIAN_MODEL=~/permanent/data/fr-en/model/marian/model.npz.decoder.yml
 
-$MOSES/scripts/tokenizer/tokenizer.perl -l $LANG | $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL | $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL | $MARIAN/build/marian_decoder -c $MARIAN_MODEL
+tee in |
+ LC_ALL=C $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG |
+ LC_ALL=C $MOSES/scripts/recaser/truecase.perl -model $TC_MODEL |
+ $SUBWORD_NMT/subword_nmt/apply_bpe.py -c $BPE_MODEL | tee in.2 |
+ $MARIAN/build/marian-decoder -c $MARIAN_MODEL --beam-size 1 --mini-batch 1 --maxi-batch 1 |
+ tee out
+
+
 
