ae5a3e90092c65fafd6d7e159e71e89b8dffa63a
Hieu Hoang <hieuhoang@gmail.com>
2018-11-14 18:57:54 +0000

    incorrect variable name in scripts. No translate.sh runs

--- snakemake/translate.sh
+++ snakemake/translate.sh
@@ -7,6 +7,6 @@ PERMANENTDIR=$3
 
 cat /dev/stdin > $DIRNAME/in
 snakemake --snakefile Snakefile.nmt --directory $DIRNAME --configfile $CONFIGFILE \
-          --config permanentDir=$nmtPermanentDir -k -j 4 translate_only
+          --config permanentDir=$PERMANENTDIR -k -j 4 translate_only
 
 cat $DIRNAME/evaluation/output
