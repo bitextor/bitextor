996ac20f7f478909a7c7ebce351eca7138b0b5f7
Hieu Hoang <hieuhoang@gmail.com>
2018-11-15 15:44:44 +0000

    use dirname to find Snakefile

--- snakemake/nmt/translate.sh
+++ snakemake/nmt/translate.sh
@@ -1,12 +1,14 @@
 
 
+THIS_DIR=$(dirname "$0")
+
 CONFIGFILE=$1
 WORK_DIR=$2
 PERMANENT_NMT_DIR=$3
-#echo $DIRNAME
+#echo $WORK_DIR
 
 cat /dev/stdin > $WORK_DIR/in
-snakemake --snakefile Snakefile.nmt --directory $WORK_DIR --configfile $CONFIGFILE \
+snakemake --snakefile $THIS_DIR/Snakefile --directory $WORK_DIR --configfile $CONFIGFILE \
           --config permanentDir=$PERMANENT_NMT_DIR -k -j 4 translate_only
 
 cat $WORK_DIR/evaluation/output
