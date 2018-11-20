a90619359d6fcfbdad885501d8e11a1dbea89383
Hieu Hoang <hieuhoang@gmail.com>
2018-11-20 15:02:12 +0000

    install libsparsehash-dev for (old?) marian

--- slurm/install.sh
+++ slurm/install.sh
@@ -32,7 +32,7 @@ installdependencies(){
     sudo apt-get update
 
     sudo apt-get install -y g++ make python3 python3-pip libbz2-dev liblzma-dev zlib1g-dev libicu-dev python-dev
-    sudo apt-get install -y automake pkg-config openjdk-8-jdk python3-magic maven nfs-kernel-server nfs-common parallel sshpass emacs munge slurm-wlm ubuntu-drivers-common apt-transport-https azure-cli cuda httrack libcld2-dev &
+    sudo apt-get install -y automake pkg-config openjdk-8-jdk python3-magic maven nfs-kernel-server nfs-common parallel sshpass emacs munge slurm-wlm ubuntu-drivers-common apt-transport-https azure-cli cuda httrack libcld2-dev libsparsehash-dev &
 
     sudo pip3 install --upgrade python-Levenshtein tensorflow keras iso-639 langid nltk regex h5py warc3-wet snakemake tld tldextract tqdm lxml html5lib ftfy bs4 toolwrapper docopt openfile pycld2 sklearn &
 
