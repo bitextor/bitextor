740488a909d724b6725c6ade0cfbf08f0f2cff57
Leopoldo Pla <leopoldoplasempere@gmail.com>
2018-11-26 14:22:19 +0100

    Boilerpipe was reverted to avoid using 'mvn' (and, at some point, mvn will be deleted along with tika)

--- piped-boilerpipe/src/pipedboilerpipe/PipedBoilerpipe.java
+++ piped-boilerpipe/src/pipedboilerpipe/PipedBoilerpipe.java
@@ -1,4 +1,10 @@
-package com.bitextor.pipedboilerpipe;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pipedboilerpipe;
 
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.document.TextDocument;
