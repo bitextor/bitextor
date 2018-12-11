cfe14bdca09f157dc8b209cad73d8aebafb1a9e3
Hieu Hoang <hieuhoang@gmail.com>
2018-12-11 05:45:30 +0000

    protect again extraneous 'Binary file (standard input) matches' at the end of stream

--- bitextor-dir2warc.py
+++ bitextor-dir2warc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os.path
 import re
 import sys
 import argparse
@@ -12,18 +13,19 @@ reader = sys.stdin
 
 for fline in reader:
     filepath=fline.strip()
-    content=None
-    url=None
-    with open(filepath, 'rb') as content_file:
-      content = content_file.read()
-    for line in content.split(b"\n"):
-      if re.search(rb'<!-- Mirrored from ', line):
-        url = re.sub(rb'.*<!-- Mirrored from ', b'', re.sub(rb' by HTTrack Website Copier.*', b'', line))
-        break
-    if url == None:
-      warc_record = warc.WARCRecord(payload=content,headers={"WARC-Target-URI":"unknown"})
-    else:
-      warc_record = warc.WARCRecord(payload=content,headers={"WARC-Target-URI":url.decode("utf8")})
-    f = warc.WARCFile(fileobj=sys.stdout.buffer)
-    f.write_record(warc_record)
+    if os.path.isfile(filepath): # protect again extraneous 'Binary file (standard input) matches' at the end of stream
+        content=None
+        url=None
+        with open(filepath, 'rb') as content_file:
+            content = content_file.read()
+        for line in content.split(b"\n"):
+          if re.search(rb'<!-- Mirrored from ', line):
+              url = re.sub(rb'.*<!-- Mirrored from ', b'', re.sub(rb' by HTTrack Website Copier.*', b'', line))
+              break
+        if url == None:
+            warc_record = warc.WARCRecord(payload=content,headers={"WARC-Target-URI":"unknown"})
+        else:
+            warc_record = warc.WARCRecord(payload=content,headers={"WARC-Target-URI":url.decode("utf8")})
+        f = warc.WARCFile(fileobj=sys.stdout.buffer)
+        f.write_record(warc_record)
 
