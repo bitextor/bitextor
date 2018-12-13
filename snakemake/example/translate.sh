851b355d02eb827af682259eb8866ada77c7ad76
Hieu Hoang <hieuhoang@gmail.com>
2018-12-13 19:39:25 +0000

    use moses

--- snakemake/example/translate.sh
+++ snakemake/example/translate.sh
@@ -1,2 +1,2 @@
 cd date >> /tmp/signal
-cat /dev/stdin | tr '[' '(' | tr ']' ')' | /home/lpla/permanent/mosesdecoder/bin/moses2 -f /home/lpla/permanent/fast-fr-en/moses.ini
+cat /dev/stdin | tr '[' '(' | tr ']' ')' | /home/lpla/permanent/mosesdecoder/bin/moses -f /home/lpla/permanent/fast-fr-en/moses.ini
