6ecfe1413f5809abeb0452979572cf09a2cfeb01
Lesbinary <leopoldoplasempere@gmail.com>
2019-10-28 11:44:44 +0100

    Deleted bifixer.sh

--- bifixer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-INPUT_FILE=$(mktemp) 
-cat > "$INPUT_FILE"
-python3 bifixer/bifixer/bifixer.py -q "${INPUT_FILE}" "${INPUT_FILE}".o "$1" "$2" "$3"
-
-cat "${INPUT_FILE}".o
-
-rm -Rf "$INPUT_FILE" "${INPUT_FILE}".o
