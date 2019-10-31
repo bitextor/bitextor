90e84cb6cf7a114f0b014b64e553411aa042ccae
Elsa Sarrias <elsa.sar.ali@gmail.com>
2019-10-31 14:51:50 +0100

    bitextor-tokenize and extract_lett now work with only one file

--- bitextor-tokenize.py
+++ bitextor-tokenize.py
@@ -28,16 +28,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/utils")
 from utils.common import open_xz_or_gzip_or_plain
 
 
-def get_lang_or_default(dict, lang):
-    if not dict:
-        return None
-    if lang in dict:
-        return dict[lang]
-    elif "default" in dict:
-        return dict["default"]
-    else:
-        return None
-
 def extract_encoded_text(encoded, sent_tokeniser, word_tokeniser, morph_analyser):
     if not sent_tokeniser:
         return encoded
@@ -68,70 +58,15 @@ def extract_encoded_text(encoded, sent_tokeniser, word_tokeniser, morph_analyser
 
 oparser = argparse.ArgumentParser(
     description="Tool that tokenizes (sentences, tokens and morphemes) plain text")
-oparser.add_argument('--folder', dest='folder', help='Bitextorlang folder', required=True)
-oparser.add_argument('--langs', dest='langs', default=None,
-                     help="List of  two-character language codes (comma-separated) to tokenize. "
-                          "If not specified, every language will be processed")
-oparser.add_argument('--sentence-splitters', dest='splitters', required=True,
-                     help="A map of sentence splitter commands. "
-                          "Format: {\"lang1\": \"script1\", ... , \"langN\": \"scriptN\", \"default\": \"defaultScript\"}. "
-                          "For languages that are not in this map but are in 'langs', the defaultScript will be used. "
-                          "If defaultScript is not specified, language will be outputted in plain text.")
-oparser.add_argument('--word-tokenizers', dest='tokenizers', required=True,
-                     help="A map of word tokenisation commands. "
-                          "Format: {\"lang1\": \"script1\", ... , \"langN\": \"scriptN\", \"default\": \"defaultScript\"}. "
-                          "For languages that are not in this map but are in 'langs', the defaultScript will be used. "
-                          "If defaultScript is not specified, word tokenization for that language will be omitted.")
-oparser.add_argument('--morph-analysers', dest='lemmatizers',
-                     help="A map of morphological analysers. "
-                          "Format: {\"lang1\": \"script1\", ... , \"langN\": \"scriptN\", \"default\": \"defaultScript\"}. "
-                          "For languages that are not in this map but a re in 'langs', the defaultScript will be used. "
-                          "If defaultScript is not specified, morphological analysis for that language will be omitted.")
+oparser.add_argument('--text', dest='text', help='Plain text file', required=True)
+oparser.add_argument('--sentence-splitter', dest='splitter', required=True, help="Sentence splitter commands")
+oparser.add_argument('--word-tokenizer', dest='tokenizer', required=True, help="Word tokenisation command")
+oparser.add_argument('--morph-analyser', dest='lemmatizer', help="Morphological analyser command")
 
 options = oparser.parse_args()
 
-if options.langs:
-    langs = options.langs.split(',')
-else:
-    langs = []
-
-try:
-    options.splitters = ast.literal_eval(options.splitters)
-except:
-    print("Sentence splitters incorrect format", file=sys.stderr)
-    sys.exit(1)
-
-try:
-    options.tokenizers = ast.literal_eval(options.tokenizers)
-except:
-    print("Word tokenizers incorrect format", file=sys.stderr)
-    sys.exit(1)
-
-try:
-    if options.lemmatizers:
-        options.lemmatizers = ast.literal_eval(options.lemmatizers)
-except:
-    print("Morphological analysers incorrect format")
-    sys.exit(1)
-
-lang_files = {}
-
-folder = os.fsencode(options.folder)
-for langfolder in os.listdir(folder):
-    lang = os.fsdecode(langfolder)
-    if not os.path.isdir(options.folder+"/"+lang) or len(lang) > 2:
-        continue
-    fullname = os.path.join(options.folder, lang+"/plain_text.xz")
-    if os.path.isfile(fullname) and (not langs or lang in langs):
-        if lang not in lang_files:
-            lang_files[lang] = lzma.open(os.path.join(options.folder, lang + "/plain_tokenized.xz"), "wb")
-        with open_xz_or_gzip_or_plain(fullname) as text_reader:
-            for line in text_reader:
-                encodedtext = line.strip()
-                senttok = get_lang_or_default(options.splitters, lang)
-                wordtok = get_lang_or_default(options.tokenizers, lang)
-                morphtok = get_lang_or_default(options.lemmatizers, lang)
-                tokenized = extract_encoded_text(encodedtext, senttok, wordtok, morphtok)
-                lang_files[lang].write("{}\n".format(tokenized).encode("utf-8"))
-for lang in lang_files:
-    lang_files[lang].close()
+with open_xz_or_gzip_or_plain(options.text) as reader:
+    for line in reader:
+        encoded_text = line.strip()
+        tokenized = extract_encoded_text(encoded_text, options.splitter, options.tokenizer, options.lemmatizer)
+        print(tokenized)
--- document-aligner/utils/extract_lett.py
+++ document-aligner/utils/extract_lett.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-
-
-try:
-    import lzma
-except ImportError:
-    from backports import lzma
 #  This file is part of Bitextor.
 #
 #  Bitextor is free software: you can redistribute it and/or modify
@@ -22,27 +16,15 @@ except ImportError:
 
 import argparse
 import base64
-import gzip
 import os
 import string
 import sys
 import html
-import ast
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../utils")
 from external_processor import ExternalTextProcessor
 from common import open_xz_or_gzip_or_plain
 
-def get_name(output_dir, prefix, language, tokenized, xz):
-    if tokenized:
-        basename = "{0}{1}.extracted.tokenized".format(prefix, language)
-    else:
-        basename = "{0}{1}.extracted".format(prefix, language)
-
-    if xz:
-        return os.path.join(output_dir, basename+".xz")
-    else:
-        return os.path.join(output_dir, basename+".gz")
 
 def filter_digits_and_punctuation(original_text):
     text_split = original_text.split()
@@ -67,107 +49,50 @@ def split_sentences(original_text, sentence_splitter_cmd):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lang1", dest="l1", default="", help="Source language")
-    parser.add_argument("--lang2", dest="l2", default="", help="Target language")
-    parser.add_argument("--langs", dest="languages", default="",
-                        help="Languages to be extracted (comma-separated). "
-                             "If non are specified, all the languages found will be extracted.")
-    parser.add_argument("--splitters", dest="splitters", default="",
-                        help="A map of sentence splitter commands. "
-                             "Format: {\"lang1\": \"script1\", ... , \"langN\": \"scriptN\", \"default\": \"defaultScript\"}. "
-                             "For languages that are not in this map but are in 'langs', the defaultScript will be used. "
-                             "If defaultScript is not specified, language will be omitted.")
+    parser.add_argument("--text", dest="text_file", required=True,
+                        help='File containing the plain text extracted from the HTML documents in a WARC file, '
+                             'encoded in base64')
+    parser.add_argument("--url", dest="url_file", required=True,
+                        help="File containing the list of urls of the documents in a WARC file")
+    parser.add_argument("--splitter", dest="splitter", default="",
+                        help="Sentence splitting command")
     parser.add_argument("--tokenized", dest="tokenized", action="store_true",
-                        help='Don\'t apply sentence splitter to the text (split by newlines only). '
-                             'Output files will be named <lang>.extracted.tokenized.gz')
-    parser.add_argument("--output_prefix", dest="output_prefix", default="",
-                        help="Prefix for output files within directory", required=False)
-    parser.add_argument("--output_dir", dest="output_dir", default=".",
-                        help="Output directory. Extracted files will be named as <lang>.extracted.gz", required=False)
-    parser.add_argument("--prune", dest="prune_threshold", type=int,
-                        default=80, help="Prune sentences longer than n (words/characters)", required=False)
+                        help='Don\'t apply sentence splitter to the text (split by newlines only).')
+    parser.add_argument("--output_prefix", dest="output_prefix", default="", required=False,
+                        help="Prefix for output files within directory")
+    parser.add_argument("--prune", dest="prune_threshold", type=int, default=80,
+                        help="Prune sentences longer than n (words/characters)", required=False)
     parser.add_argument("--prune_type", dest="prune_type", choices={"words", "chars"},
-                        default="words", help="Prune sentences either by words or charaters", required=False)
-    parser.add_argument("-x", "--xz", dest="xz", action="store_true",
-                        help="Use xz as the compression tool")
-    parser.add_argument('--bitextorlang', dest='bitextorlang', help='Bitextorlang folder')
-
+                        default="words", help="Prune sentences either by words or characters", required=False)
     args = parser.parse_args()
 
-    if not args.tokenized:
-        try:
-            args.splitters = ast.literal_eval(args.splitters)
-        except:
-            print("Sentence splitters incorrect format", file=sys.stderr)
-            sys.exit(1)
+    with open_xz_or_gzip_or_plain(args.text_file) as text_reader, \
+            open_xz_or_gzip_or_plain(args.url_file) as url_reader:
+        for line in text_reader:
+            text = base64.b64decode(line.strip()).decode("utf-8")
+            uri = next(url_reader, None).strip()
 
-    if args.languages != "":
-        langs_parse = args.languages.strip().split(',')
-    else:
-        langs_parse = []
+            if not text:
+                continue
 
-    # make sure that if languages are specified, lang1 and lang2 are among them
-    if langs_parse:
-        if args.l1 and args.l1 not in langs_parse:
-            langs_parse.append(args.l1)
-        if args.l2 and args.l2 not in langs_parse:
-            langs_parse.append(args.l2)
+            if args.tokenized:
+                split = split_sentences(text, None)
+            else:
+                split = split_sentences(text, args.splitter)
 
-    lang_file = {}
-    if args.tokenized:
-        text_file_name = "/plain_tokenized.xz"
-    else:
-        text_file_name = "/plain_text.xz"
-    folder = os.fsencode(args.bitextorlang)
-    for lang in os.listdir(folder):
-        lang = os.fsdecode(lang)
-        if not os.path.isdir(args.bitextorlang + "/" + lang) or len(lang) > 2:
-            continue
-        if not lang.strip():
-            continue
-        if langs_parse and lang not in langs_parse:
-            continue
+            for extracted_line in split:
 
-        lang_file[lang] = lzma.open(get_name(args.output_dir, args.output_prefix, lang, args.tokenized, args.xz), "wb")
-
-        with open_xz_or_gzip_or_plain(args.bitextorlang+"/"+lang+text_file_name) as text_reader, \
-                open_xz_or_gzip_or_plain(args.bitextorlang+"/"+lang+"/url.xz") as url_reader:
-            for line in text_reader:
-                text = base64.b64decode(line.strip()).decode("utf-8")
-                uri = next(url_reader, None).strip()
-
-                if not text:
+                extracted_line = extracted_line.strip()
+                if not extracted_line:
                     continue
 
-                if args.tokenized:
-                    split = split_sentences(text, None)
-                elif lang in args.splitters:
-                    split = split_sentences(text, args.splitters[lang])
-                elif "default" in args.splitters:
-                    split = split_sentences(text, args.splitters["default"])
-                else:
-                    continue
-
-                for extracted_line in split:
-
-                    extracted_line = extracted_line.strip()
-                    if not extracted_line:
+                # prune long sentences
+                extracted_line = extracted_line
+                if args.prune_type == "chars":
+                    if len(extracted_line) > args.prune_threshold:
+                        continue
+                elif args.prune_type == "words":
+                    if len(extracted_line.split()) > args.prune_threshold:
                         continue
 
-                    # prune long sentences
-                    extracted_line = extracted_line
-                    if args.prune_type == "chars":
-                        if len(extracted_line) > args.prune_threshold:
-                            continue
-                    elif args.prune_type == "words":
-                        if len(extracted_line.split()) > args.prune_threshold:
-                            continue
-
-                    if lang not in lang_file:
-                        lang_file[lang] = lzma.open(get_name(args.output_dir, args.output_prefix, lang, args.tokenized, args.xz), "wb")
-
-                    lang_file[lang].write("{0}\t{1}\n".format(uri, extracted_line).encode("utf-8"))
-
-    # print("lang_file", lang_file)
-    for f in lang_file:
-        lang_file[f].close()
+                print("{0}\t{1}".format(uri, extracted_line))
