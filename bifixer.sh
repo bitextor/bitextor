d6a82876c5ae5d2678e614b3aadb5529bc36e0a8
Miquel <mespla@dlsi.ua.es>
2019-11-05 17:14:18 +0100

    Fixing missing prefix for bifixer.sh

--- bifixer.sh
+++ bifixer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
+CURPATH=$(dirname $(readlink -f $0))
 INPUT_FILE=$(mktemp) 
 cat > "$INPUT_FILE"
-python3 bifixer/bifixer/bifixer.py "${INPUT_FILE}" "${INPUT_FILE}".o "$1" "$2" "$3"
+python3 $CURPATH/bifixer/bifixer/bifixer.py "${INPUT_FILE}" "${INPUT_FILE}".o "$1" "$2" "$3"
 
 cat "${INPUT_FILE}".o
 
