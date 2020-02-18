c221bb0c3d246cc6691db3bb01f7863c79bd806c
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-02-18 19:42:33 +0000

    producer/consumer style multithreading around document alignment part

--- document-aligner/main.cpp
+++ document-aligner/main.cpp
@@ -2,12 +2,79 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <thread>
+#include <queue>
+#include <mutex>
 #include "boost/program_options.hpp"
 
 using namespace bitextor;
 using namespace std;
 
 namespace po = boost::program_options;
+
+void print_score(float score, Document const &left, Document const &right)
+{
+	// TODO: Don't print concurrently
+	cout << score
+	     << '\t' << left.url
+	     << '\t' << right.url
+	     << '\n';
+}
+
+template <typename T> class blocking_queue
+{
+public:
+	blocking_queue(size_t capacity);
+	
+	void push(T const &item);
+	void push(T &&item);
+	T pop(); // TODO: explicit move semantics?
+private:
+	size_t _size;
+	queue<T> _buffer;
+	mutex _mutex;
+	condition_variable _added;
+	condition_variable _removed;
+};
+
+template <typename T> blocking_queue<T>::blocking_queue(size_t size) : _size(size) {
+	//
+}
+
+template <typename T> void blocking_queue<T>::push(T &&item) {
+	unique_lock<mutex> mlock(_mutex);
+
+	while (_buffer.size() >= _size)
+		_removed.wait(mlock);
+
+	_buffer.push(std::move(item));
+	mlock.unlock();
+	_added.notify_one();
+}
+
+template <typename T> void blocking_queue<T>::push(T const &item) {
+	unique_lock<mutex> mlock(_mutex);
+	
+	while (_buffer.size() >= _size)
+		_removed.wait(mlock);
+	
+	_buffer.push(item);
+	mlock.unlock();
+	_added.notify_one();
+}
+
+template <typename T> T blocking_queue<T>::pop() {
+	std::unique_lock<std::mutex> mlock(_mutex);
+	
+	while (_buffer.empty())
+		_added.wait(mlock);
+	
+	T value = std::move(_buffer.front());
+	_buffer.pop();
+	mlock.unlock();
+	_removed.notify_one();
+	return value;
+}
 
 int main(int argc, char *argv[]) {
 	po::positional_options_description arg_desc;
@@ -92,31 +159,78 @@ int main(int argc, char *argv[]) {
 	
 	size_t n = 0;
 	
-	size_t hits = 0;
-
-	while (en_tokens_in >> buffer) {
+	atomic<size_t> hits(0);
+	
+	vector<thread> consumers;
+	
+	size_t n_threads = 4;
+	
+	blocking_queue<Document> queue(n_threads * 4);
+	
+	for (size_t n = 0; n < n_threads; ++n)
+		consumers.push_back(thread([&queue, &documents, &hits, threshold]() {
+			while (true) {
+				Document buffer = queue.pop();
+				
+				// Empty doc is poison
+				if (buffer.wordvec.empty())
+					break;
+				
+				for (auto const &document : documents) {
+					float score = calculate_alignment(document, buffer);
+					
+					if (score >= threshold)
+						// print_score(score, document, buffer);
+						++hits;
+				}
+			}
+		}));
+	
+	auto stop = [&consumers, &queue, n_threads]() {
+		// Send poison to all workers
+		for (size_t n = 0; n < n_threads; ++n)
+			queue.push(Document());
+		
+		// Wait for the workers to finish
+		for (auto &consumer : consumers)
+			consumer.join();
+	};
+	
+	while (true) {
+		Document buffer;
+		
+		if (!(en_tokens_in >> buffer))
+			break;
+		
 		if (!(en_urls_in >> buffer.url)) {
 			cerr << "Error while reading url for the " << n << "th document" << endl;
+			stop();
 			return 3;
 		}
 		
 		++n;
 		
+		if (buffer.vocab.empty()) {
+			cerr << "Document " << n << " resulted in an empty vocab" << endl;
+			stop();
+			return 4;
+		}
+		
 		calculate_tfidf(buffer, documents.size(), df);
 		
-		for (auto const &document : documents) {
-			float score = calculate_alignment(document, buffer);
-			
-			if (score < threshold)
-				continue;
-			
-			++hits;
-//			cout << score
-//				 << '\t' << document.url
-//				 << '\t' << buffer.url
-//				 << '\n';
+		// Make 100% sure it is not an empty document, empty docs are poisonous!
+		if (buffer.wordvec.empty()) {
+			cerr << "Document " << n << " resulted in an empty word vec" << endl;
+			stop();
+			return 5;
 		}
+		
+		// Push this document to the alignment score calculators
+		queue.push(std::move(buffer));
 	}
 	
-	cout << hits << endl;
+	stop();
+	
+	// Tada!
+	cout << hits.load() << endl;
 }
