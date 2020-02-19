4b262e2e85e8eb669854ff2b5ed5272f29f292af
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-02-19 11:13:55 +0000

    Make it cheaper to move memory around
    Because those vectors will grow and grow

--- document-aligner/main.cpp
+++ document-aligner/main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <queue>
 #include <mutex>
+#include <memory>
 #include "boost/program_options.hpp"
 
 using namespace bitextor;
@@ -101,7 +102,7 @@ int main(int argc, char *argv[]) {
 		cerr << exception.what() << endl;
 		return 1;
 	}
-		
+	
 	if (vm.count("help")
 		|| !vm.count("translated-tokens") || !vm.count("translated-urls")
 		|| !vm.count("english-tokens") || !vm.count("english-urls"))
@@ -114,20 +115,23 @@ int main(int argc, char *argv[]) {
 	
 	// Read first set of documents into memory.
 
-	std::vector<Document> documents;
+	std::vector<unique_ptr<Document>> documents;
 	
 	ifstream tokens_in(vm["translated-tokens"].as<std::string>());
 	ifstream urls_in(vm["translated-urls"].as<std::string>());
 
-	Document buffer;
-
-	while (tokens_in >> buffer) {
-		if (!(urls_in >> buffer.url)) { // TODO: Dangerous assumption that there is no space in url
+	while (true) {
+		unique_ptr<Document> buffer(new Document());
+		
+		if (!(tokens_in >> *buffer))
+			break;
+		
+		if (!(urls_in >> buffer->url)) { // TODO: Dangerous assumption that there is no space in url
 			cerr << "Error while reading the url for the " << documents.size() << "th document" << endl;
 			return 2;
 		}
 
-		documents.push_back(buffer);
+		documents.push_back(std::move(buffer));
 	}
 
 	cerr << "Read " << documents.size() << " documents" << endl;
@@ -137,7 +141,7 @@ int main(int argc, char *argv[]) {
 	map<NGram,size_t> df;
 	
 	for (auto const &document : documents)
-		for (auto const &entry : document.vocab)
+		for (auto const &entry : document->vocab)
 			df[entry.first] += 1;
 	
 	cerr << "Aggregated DF" << endl;
@@ -146,10 +150,10 @@ int main(int argc, char *argv[]) {
 	
 	for (auto &document : documents) {
 		// Turn the vocab map into a sorted tfidf score list
-		calculate_tfidf(document, documents.size(), df);
+		calculate_tfidf(*document, documents.size(), df);
 		
 		// Save a bit of memory
-		document.vocab.clear();
+		document->vocab.clear();
 	}
 	
 	cerr << "Calculated translated TFIDF scores" << endl;
@@ -170,19 +174,19 @@ int main(int argc, char *argv[]) {
 	
 	size_t n_threads = 4;
 	
-	blocking_queue<Document> queue(n_threads * 4);
+	blocking_queue<unique_ptr<Document>> queue(n_threads * 4);
 	
 	for (size_t n = 0; n < n_threads; ++n)
 		consumers.push_back(thread([&queue, &documents, &hits, threshold]() {
 			while (true) {
-				Document buffer = queue.pop();
+				unique_ptr<Document> buffer(queue.pop());
 				
 				// Empty doc is poison
-				if (buffer.wordvec.empty())
+				if (!buffer)
 					break;
 				
 				for (auto const &document : documents) {
-					float score = calculate_alignment(document, buffer);
+					float score = calculate_alignment(*document, *buffer);
 					
 					if (score >= threshold)
 						// print_score(score, document, buffer);
@@ -194,7 +198,7 @@ int main(int argc, char *argv[]) {
 	auto stop = [&consumers, &queue, n_threads]() {
 		// Send poison to all workers
 		for (size_t n = 0; n < n_threads; ++n)
-			queue.push(Document());
+			queue.push(nullptr);
 		
 		// Wait for the workers to finish
 		for (auto &consumer : consumers)
@@ -202,12 +206,12 @@ int main(int argc, char *argv[]) {
 	};
 	
 	while (true) {
-		Document buffer;
+		unique_ptr<Document> buffer(new Document());
 		
-		if (!(en_tokens_in >> buffer))
+		if (!(en_tokens_in >> *buffer))
 			break;
 		
-		if (!(en_urls_in >> buffer.url)) {
+		if (!(en_urls_in >> buffer->url)) {
 			cerr << "Error while reading url for the " << n << "th document" << endl;
 			stop();
 			return 3;
@@ -215,19 +219,19 @@ int main(int argc, char *argv[]) {
 		
 		++n;
 		
-		if (buffer.vocab.empty()) {
+		if (buffer->vocab.empty()) {
 			cerr << "Document " << n << " resulted in an empty vocab" << endl;
 			stop();
 			return 4;
 		}
 		
 		// TODO: Move this into consumers as well?
-		calculate_tfidf(buffer, documents.size(), df);
+		calculate_tfidf(*buffer, documents.size(), df);
 		
-		buffer.vocab.clear();
+		buffer->vocab.clear();
 		
 		// Make 100% sure it is not an empty document, empty docs are poisonous!
-		if (buffer.wordvec.empty()) {
+		if (buffer->wordvec.empty()) {
 			cerr << "Document " << n << " resulted in an empty word vec" << endl;
 			stop();
 			return 5;
