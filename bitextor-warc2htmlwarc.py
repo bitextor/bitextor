56e06ea1d0f5c1b581c782cdbc9c1734e003ff56
Kenneth Heafield <github@kheafield.com>
2020-02-21 21:40:49 +0000

    Spelling of verbatim

--- bitextor-warc2htmlwarc.py
+++ bitextor-warc2htmlwarc.py
@@ -117,7 +117,7 @@ oparser.add_argument('--output', dest='output', help='Output WARC file', default
 oparser.add_argument('--input', dest='input', help='Input WARC file', default=sys.stdin)
 oparser.add_argument('--pdfextract', action="store_true", help='Use pdf-extract engine or pdftohtml for PDFs',
                      default=False)
-oparser.add_argument('--pdfpass', dest='pdfpass', help='Pass PDFs verbatime to file', default=None)
+oparser.add_argument('--pdfpass', dest='pdfpass', help='Pass PDFs verbatim to file', default=None)
 oparser.add_argument('--ftfy', action='store_true', help='User fix-text-for-you to fix possible encoding problems',
                     default=False)
 oparser.add_argument('--cleanhtml', action='store_true', help='Clean HTML to remove javascript, css and head tags',
