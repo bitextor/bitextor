4c561ddb0423775fcf335a1705e1a2449b45bb26
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-02-25 13:45:15 +0000

    Remove base64_encode legacy wrapper

--- document-aligner/base64.h
+++ document-aligner/base64.h
@@ -8,11 +8,4 @@ void base64_encode(const StringPiece &in, std::string &out);
 
 void base64_decode(const StringPiece &in, std::string &out);
 
-// TODO: delete these functions.
-inline std::string base64_encode(const StringPiece &in) {
-	std::string ret;
-	base64_encode(in, ret);
-	return ret;
-}
-
 }
