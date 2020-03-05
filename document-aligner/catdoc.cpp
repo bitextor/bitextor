9e86ff90b119dbbf0448718d74f4229d0c4890ea
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-03-05 12:45:43 +0000

    Give catdoc the option to include the indices as well
    Might be more useful than the urls in some cases.

--- document-aligner/catdoc.cpp
+++ document-aligner/catdoc.cpp
@@ -34,17 +34,19 @@ ostream &operator<<(ostream &out, Row const &row) {
 
 typedef vector<unique_ptr<util::FilePiece>> FileSet;
 
-bool skip_row(FileSet &files) {
+bool skip_rows(FileSet &files, size_t n) {
 	StringPiece line;
 
 	for (auto &file : files)
-		if (!file->ReadLineOrEOF(line))
-			return false;
+		for (size_t i = 0; i < n; ++i)
+			if (!file->ReadLineOrEOF(line))
+				return false;
 
 	return true;
 }
 
 bool read_row(FileSet &files, Row &row) {
+	row.cells.clear();
 	row.cells.reserve(files.size());
 
 	StringPiece line;
@@ -58,15 +60,54 @@ bool read_row(FileSet &files, Row &row) {
 	return true;
 }
 
+enum Source {
+	LEFT,
+	RIGHT,
+	LEFT_INDEX,
+	RIGHT_INDEX
+};
+
+int usage(char *progname) {
+	cout << "Usage: " << progname << " [ -l filename | -r filename | -li | -ri ] ...\n"
+	        "Input via stdin: <float> \"\\t\" <left index> \"\\t\" <right index> \"\\n\"\n"
+	        "\n"
+	        "This program joins rows from two sets of files into tab-separated output.\n"
+	      	"\n"
+	        "Column options:\n"
+	        "  -l    Use left index for the following files\n"
+	        "  -r    Use right index for the following files\n"
+	        "  -li   Print the left index\n"
+	        "  -ri   Print the right index\n"
+	        "\n"
+	        "The order of the columns in the output is the same as the order of the\n"
+	        "arguments given to the program.\n";
+	return 127;
+}
+
 int main(int argc, char *argv[]) {
-	// Open the files on the left and right side of the join
+	if (argc <= 1)
+		return usage(argv[0]);
+
+	// Open the files on the left and right side of the join. Keep track of
+	// which file is on the left and on the right side, but also of the order
+	// of the arguments as this will dictate the order of the output.
 	FileSet left_files, right_files;
-	FileSet *files = &left_files;
+	vector<Source> order;
+	Source side = LEFT;
+	FileSet *files[]{&left_files, &right_files};
 	for (int pos = 1; pos < argc; ++pos) {
-		if (argv[pos] == string("--"))
-			files = &right_files;
-		else
-			files->emplace_back(new util::FilePiece(argv[pos]));
+		if (string(argv[pos]) == "-l")
+			side = LEFT;
+		else if (string(argv[pos]) == "-r")
+			side = RIGHT;
+		else if (string(argv[pos]) == "-li")
+			order.push_back(LEFT_INDEX);
+		else if (string(argv[pos]) == "-ri")
+			order.push_back(RIGHT_INDEX);
+		else {
+			files[side]->emplace_back(new util::FilePiece(argv[pos]));
+			order.push_back(side);
+		}
 	}
 
 	// Read our joins into memory
@@ -103,16 +144,16 @@ int main(int argc, char *argv[]) {
 		assert(join_it->right_index >= right_index);
 
 		// While our index is still far off, skip rows
-		while (join_it->right_index - 1 > right_index) {
-			if (!skip_row(right_files)) {
+		if (right_index < join_it->right_index - 1) {
+			if (!skip_rows(right_files, (join_it->right_index - 1) - right_index)) {
 				cerr << "Right index " << join_it->right_index << " outside of range " << right_index << endl;
 				return 1;
 			}
-			++right_index;
+			right_index = join_it->right_index - 1;
 		}
 
 		// Next row is the row we want, read it.
-		if (join_it->right_index - 1 == right_index) {
+		if (right_index == join_it->right_index - 1) {
 			if (!read_row(right_files, right_row)) {
 				cerr << "Right index " << join_it->right_index << " outside of range " << right_index << endl;
 				return 1;
@@ -126,7 +167,27 @@ int main(int argc, char *argv[]) {
 			return 2;
 		}
 
-		cout << left_rows[join_it->left_index - 1] << '\t' << right_row << endl;
+		// Print the columns in the order in which the input files were given to the program
+		for (size_t i = 0, l = 0, r = 0; i < order.size(); ++i) {
+			if (i > 0)
+				cout << '\t';
+			
+			switch (order[i]) {
+				case LEFT:
+					cout << left_rows[join_it->left_index - 1].cells[l++];
+					break;
+				case RIGHT:
+					cout << right_row.cells[r++];
+					break;
+				case LEFT_INDEX:
+					cout << join_it->left_index;
+					break;
+				case RIGHT_INDEX:
+					cout << join_it->right_index;
+					break;
+			}
+		}
+		cout << endl;
 	}
 
 	return 0;
