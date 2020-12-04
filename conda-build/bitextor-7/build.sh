d14949a3d15e06c660857d290da1604590f69cfe
Cristian García Romero <cgr71ii@gmail.com>
2020-12-04 14:31:55 +0100

    linguacrawl integration, conda build and fixes
    The crawler linguacrawl has been integrated, and those options which
    were only creepy-specific, now are shared (new configuration options
    has been added as well). Due to the fact that linguacrawl is far
    different from the others crawlers, specific logic has been needed
    in order to be correctly integrated. Conda build scripts ready to
    generate Bitextor builds (Bitextor 7, which is in master right now,
    and 8, which is the current branch). Those scripts allows to
    automatizate completely the generation of a build for Bitextor,
    resulting in a file which might be uploaded (is the idea) to the
    official repositories of Anaconda. These scripts attempt to handle
    some known problems, but even so, problems might happen due to the
    fact that is a first version. Different fixes have been applied:
    relative call to ~/go/bin has been replaced with a relative call to
    the binaries themselves, but in the case of not find the relative
    binaries, the initial call will be made; in those cases when no
    common shards were generated, neither segalign or docalign were
    executed in any of the possible configurations, and this only happens
    in the case of very different URLs and bad luck because no shard id
    was equal in any of the provided languages, and in order to fix this
    situation, a fake shard is generated only if is necessary, and after
    the generation of this shard, the rules will be updated and lead to
    a correct execution of the pipeline, avoiding to break; Biroamer
    information was being discarted, which initially was intentional, but
    it is not the best due to the fact that in some cases fails, but
    finish correctly, and we could have though that it finished correctly
    when did not

---
+++ conda-build/bitextor-7/build.sh
@@ -0,0 +1,141 @@
+
+echo -e "\e[4m\e[1mBuilding Bitextor...\e[0m"
+
+cd "bitextor"
+
+# Master cmake fix
+docalign_cmakelists_icu=$(cat document-aligner/CMakeLists.txt | grep {ICU_INCLUDE_DIR})
+
+if [[ "$docalign_cmakelists_icu" != "" ]]; then
+  sed -i 's/{ICU_INCLUDE_DIR}/{ICU_INCLUDE_DIRS}/' document-aligner/CMakeLists.txt
+  sed -i ':a;N;$!ba;s/{ICU_INCLUDE_DIRS}\n)/{ICU_INCLUDE_DIRS}\n)\nMESSAGE("ICU_INCLUDE_DIRS: " ${ICU_INCLUDE_DIRS})/' document-aligner/CMakeLists.txt
+  sed -i ':a;N;$!ba;s/set(CMAKE_CXX_FLAGS "-Wall")/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")/' document-aligner/CMakeLists.txt
+fi
+
+# Go dependencies
+echo -e " - \e[4mInstalling Go dependencies...\e[0m"
+mkdir "$PWD/../gopath"
+go env
+#export CGO_ENABLED="1"
+export GOPATH="$PWD/../gopath"
+go get github.com/paracrawl/giawarc/...
+#go get github.com/paracrawl/giashard/...
+
+# Pip dependencies
+echo -e " - \e[4mInstalling python3 dependencies...\e[0m"
+export PIP_NO_INDEX="False" # We are downloading requisites from PyPi
+export PIP_NO_DEPENDENCIES="False" # We need the dependencies from our defined dependencies
+pip3 install -r requirements.txt
+pip3 install -r bicleaner/requirements.txt
+pip3 install https://github.com/kpu/kenlm/archive/master.zip --install-option="--max_order 7"
+pip3 install -r bifixer/requirements.txt
+
+# CLD3
+echo -e " - \e[4mInstalling CLD3...\e[0m"
+cd ..
+tmp_dir="$(mktemp -d -t cld3-XXXXXXXXXX)"
+cwd="$PWD"
+cd "$tmp_dir"
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protobuf-all-3.11.4.tar.gz
+tar -zxvf protobuf-all-3.11.4.tar.gz
+mv "protobuf-3.11.4" "$cwd/"
+cd "$cwd/protobuf-3.11.4"
+mkdir "$cwd/protobuf"
+./configure --prefix="$cwd/protobuf"
+make
+make check
+make install || echo -e " - \e[31m\e[4mcld3-install: something wrong happened with 'make install'...\e[0m"
+LD_LIBRARY_PATH="$cwd/protobuf/lib" ldconfig -C "$cwd/ld.so.cache" || \
+(
+  echo -e " - \e[31m\e[4mcld3-install: something wrong happened with 'ldconfig'...\e[0m";
+  echo -e " - \e[4mcld3-install: problem at '$cwd/protobuf-3.11.4' (you can try to fix it manually)...\e[0m";
+  echo -e " - \e[4mcld3-install: sleep 10800: use 'ps aux | grep sleep' and 'kill -s sigint \$pid' in order to continue...\e[0m";
+  sleep 10800;
+)
+pip3 install Cython
+pip3 install pycld3
+cd ..
+cd bitextor
+
+# Heritrix 3
+# Once installed and 'conda activate BITEXTOR_ENV': ln -s $CONDA_PREFIX/bitextor/heritrix-3.4.0-SNAPSHOT/ /opt/heritrix3
+#echo -e " - \e[4mInstalling Heritrix3...\e[0m"
+#tmp_dir="$(mktemp -d -t heritrix3-XXXXXXXXXX)"
+#cwd="$PWD"
+#cd "$tmp_dir"
+#wget http://builds.archive.org/maven2/org/archive/heritrix/heritrix/3.4.0-SNAPSHOT/heritrix-3.4.0-SNAPSHOT-dist.zip
+#unzip heritrix-3.4.0-SNAPSHOT-dist.zip
+#mv "heritrix-3.4.0-SNAPSHOT" "$cwd/"
+#cd "$cwd/heritrix-3.4.0-SNAPSHOT"
+#cd ..
+
+echo -e " - \e[4mChecking and generating files...\e[0m"
+# Execute autogen.sh and try to fix known installation issues if any
+autogen_output=$(./autogen.sh) && ok="0" || \
+  (status=$?; \
+  if [[ "$(echo $autogen_output | grep 'tensorflow for Python... no' | wc -l)" != "0" ]]; then \
+    echo -e " - \e[4mcheck-gen: trying to fix known installation issue related to TensorFlow...\e[0m"; \
+    ok="1"; \
+  else \
+    echo -e " - \e[31m\e[4mcheck-gen: unknown installation issue happened... (status: $status)\e[0m"; \
+    false; \ # Stop execution
+  fi)
+
+if [[ "$ok" == "1" ]]; then pip3 uninstall -y tensorflow && pip3 install tensorflow==2.2 keras==2.2.5; fi # tensorflow fix
+if [[ "$ok" != "0" ]]; then ./autogen.sh; fi  # Re-execute autogen.sh
+
+echo -e " - \e[4mMake...\e[0m"
+make
+
+echo -e " - \e[4mMaking post-build actions...\e[0m"
+# Clean
+find . -type d -or -type l | grep /__pycache__$ | xargs -I{} rm -rf {}
+find . -type f -or -type l | grep [.]o$ | xargs -I{} rm -rf {}
+find . -type d -or -type l | grep /CMakeFiles$ | xargs -I{} rm -rf {}
+
+# Copy src (not usual in builds, but we need src because we are using bash and python scripts)
+cp -r "$SRC_DIR/bitextor" "$PREFIX"
+cp -r "$SRC_DIR/gopath" "$PREFIX"
+cp -r "$SRC_DIR/protobuf" "$PREFIX"
+
+# Soft link (is necessary that is relative, not absolute)
+cd "$PREFIX/bin"
+
+### Create bitextor and bitextor.sh; if bitextor fails, try bitextor.sh; complain if both fails
+#ln -s ../bitextor/bitextor.sh ./bitextor && \
+#ln -s ../bitextor/bitextor.sh ./bitextor.sh || (\
+#ln -s ../bitextor/bitextor.sh ./bitextor.sh || \
+#echo "Could not create bitextor link...")
+### bitextor.sh uses dirname, so we cannot use soft links directly...
+### Other soft links
+ln -s ../gopath/bin/giawarc ./giawarc || echo -e " - \e[31m\e[4mpost-build-act: could not create link 'giawarc'...\e[0m" # Warning: bitextor will look for giarwarc at ~/go/bin
+ln -s ../protobuf/bin/protoc ./protoc || echo -e " - \e[31m\e[4mpost-build-act: could not create link 'protoc'...\e[0m"
+
+### Create an script manually and store it in order to wrap the true call
+script="/bin/sh\n\n\$(dirname \"\$0\")/../bitextor/bitextor.sh \"\$@\""
+error="0"
+scripts=( "./bitextor" "./bitextor.sh" )
+
+for f in ${scripts[@]}; do
+  if [[ ! -f "$f" ]]; then
+    echo -n "#" >> "$f" && \
+    echo -n "!" >> "$f" && \
+    echo -e "$script" >> "$f" && \
+    chmod 775 "$f" && \
+    echo -e " - \e[4mpost-build-act: link '$(basename $f)' created...\e[0m" || \
+    echo -e " - \e[31m\e[4mpost-build-act: something wrong happened... the link '$f' might have not been created...\e[0m"
+  fi
+  if [[ ! -f "$f" ]]; then
+    error=$(echo "$error+1" | bc)
+    echo -e " - \e[31m\e[4mpost-build-act: could not create link '$f'...\e[0m"
+  fi
+done
+
+if [[ "$error" == "${#scripts[@]}" ]]; then
+  echo -e " - \e[31m\e[4mpost-build-act: could not create any bitextor link[s]...\e[0m"
+fi
+
+# Restore initial working directory (trying to prevent errors)
+cd "$SRC_DIR"
+
+echo -e "\e[4m\e[1mBuild done!\e[0m"
