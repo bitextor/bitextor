3f15dc7530f39e9a677a5eb3fcffdaf790a59d6f
Cristian García Romero <cgr71ii@gmail.com>
2021-03-03 13:55:47 +0100

    Conda build nightly

---
+++ conda-build/bitextor-8-nightly/post-link.sh
@@ -0,0 +1,42 @@
+
+installation_paths=( "/opt" "$HOME" )
+
+# Create Heritrix3 soft link after the installation and inform the user about the location
+if [[ ! -d "$PREFIX/heritrix3" ]]; then
+  echo "Could not find Heritrix3 in '$PREFIX/heritrix3' (maybe is not installed, and you will need to install it manually if you want to use it)" \
+    >> "$PREFIX/.messages.txt"
+else
+  for h in "${installation_paths[@]}"; do
+    if [[ -w "$h" ]]; then
+      if [[ ! -d "$h/heritrix3" ]]; then
+        ln -s "$PREFIX/heritrix3" "$h/heritrix3"
+        echo "Your Heritrix3 path is in '$h/heritrix3'" >> "$PREFIX/.messages.txt"
+        echo "In order to use Heritrix3, check its documentation (you can find references to it at Bitextor repository)" >> "$PREFIX/.messages.txt"
+      else
+        echo "Heritrix3 installation found in '$h/heritrix3'. Soft link will not be created, but you can create it if you want using '$PREFIX/heritrix3'" \
+          >> "$PREFIX/.messages.txt"
+      fi
+
+      break
+    fi
+  done
+fi
+
+if [[ ! -d "$PREFIX/bitextor" ]]; then
+  echo "Could not find Bitextor in '$PREFIX/bitextor', and it should be (broken build?)" \
+    >> "$PREFIX/.messages.txt"
+else
+  for h in "${installation_paths[@]}"; do
+    if [[ -w "$h" ]]; then
+      if [[ ! -d "$h/bitextor" ]]; then
+        ln -s "$PREFIX/bitextor" "$h/bitextor"
+        echo "Bitextor is in '$h/bitextor'" >> "$PREFIX/.messages.txt"
+      else
+        echo "Bitextor installation found in '$h/bitextor'. Soft link will not be created, but you can create it if you want using '$PREFIX/bitextor'" \
+          >> "$PREFIX/.messages.txt"
+      fi
+
+      break
+    fi
+  done
+fi
