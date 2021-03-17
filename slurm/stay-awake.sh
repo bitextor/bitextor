69c9105721df37e0bf6e5b83a69d48f714789062
Leopoldo Pla <lpla@dlsi.ua.es>
2021-03-17 11:23:23 +0100

    Deleted deprecated slurm folder

--- slurm/stay-awake.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while :
-do
-    scontrol update NodeName=baldur,elli,gna,hel,loki,skaol,sigyn,syn,zisa State=resume reason='whatever'
-    #scontrol update NodeName=hel,loki,skaol,syn State=resume reason='whatever'
-    sleep 600
-done
