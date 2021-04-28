0ea57abc92a533c726fe09b42aacebda4077638a
Leopoldo Pla <lpla@dlsi.ua.es>
2021-04-28 14:54:35 +0200

    Added offline mode to the deferred crawling reconstructor Now positional arguments 4 and 5 can be WARC files, avoiding online connections. File based caching has also been implemented

--- deferred-annotation-reconstructor.sh
+++ deferred-annotation-reconstructor.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-zcat $1 | while read line; do
+tempcache=$(mktemp -d)
+
+zcat -f $1 | while read line; do
 	tempwarc=$(mktemp)
 	tempprocess=$(mktemp -d)
-	tempsplitsent=$(mktemp)
 	deferredhash1=$(echo "$line" | cut -f 6)
 	deferredhash2=$(echo "$line" | cut -f 7)
 	url1=$(echo "$line" | cut -f 1)
 	url2=$(echo "$line" | cut -f 2)
+	tempsplitsent1="$tempcache/$(echo $url1 | preprocess/bin/mmhsum)"
+	tempsplitsent2="$tempcache/$(echo $url2 | preprocess/bin/mmhsum)"
 	echo -n "$url1	$url2"
 	
 	
+	if [ ! -f $tempsplitsent1 ]; then
+	        if [ "$4" != "" ]; then
+			> ${tempwarc}.warc.gz
+			for id in $(warcio index $4 | grep -i "$url1" | cut -f 1 -d ',' | cut -f 2 -d ':' | cut -f 2 -d '"'); do warcio extract $4 $id | gzip >> ${tempwarc}.warc.gz; done
 	
-	wget "$url1" --quiet --warc-file $tempwarc --output-document /dev/null
-	warc2text/bin/warc2text -o $tempprocess ${tempwarc}.warc.gz 2> /dev/null
+		else
+			wget "$url1" --quiet --warc-file $tempwarc --output-document /dev/null
+		fi
 	
-	zcat $tempprocess/*/text.gz | python3 bitextor-split.py --langcode $2 > $tempsplitsent
+		warc2text/bin/warc2text -o $tempprocess ${tempwarc}.warc.gz 2> /dev/null
+		
+		zcat $tempprocess/*/text.gz | python3 bitextor-split.py --langcode $2 > $tempsplitsent1
+	fi
 
 	echo -n "	"
-	cat $tempsplitsent | base64 -d | while read linesplit; do
+	cat $tempsplitsent1 | base64 -d | while read linesplit; do
 		senthash=$(echo -n "$linesplit" | preprocess/bin/mmhsum)
 		found1=""
                 for partdeferredhash1 in $(echo "$deferredhash1" | tr "+" "\n"); do
@@ -35,14 +46,21 @@ zcat $1 | while read line; do
 	rm -rf $tempprocess
 
 
-
-	wget "$url2" --quiet --warc-file $tempwarc --output-document /dev/null
-	warc2text/bin/warc2text -o $tempprocess ${tempwarc}.warc.gz 2> /dev/null
+	if [ ! -f $tempsplitsent2 ]; then
+		if [ "$5" != "" ]; then
+			> ${tempwarc}.warc.gz
+			for id in $(warcio index $5 | grep -i "$url2" | cut -f 1 -d ',' | cut -f 2 -d ':' | cut -f 2 -d '"'); do warcio extract $5 $id | gzip >> ${tempwarc}.warc.gz; done
+		else
+			wget "$url2" --quiet --warc-file $tempwarc --output-document /dev/null
+		fi
 	
-	zcat $tempprocess/*/text.gz | python3 bitextor-split.py --langcode $3 > $tempsplitsent
+		warc2text/bin/warc2text -o $tempprocess ${tempwarc}.warc.gz 2> /dev/null
+		
+		zcat $tempprocess/*/text.gz | python3 bitextor-split.py --langcode $3 > $tempsplitsent2
+	fi
 	
 	echo -n "	"
-	cat $tempsplitsent | base64 -d | while read linesplit; do
+	cat $tempsplitsent2 | base64 -d | while read linesplit; do
 		senthash=$(echo -n "$linesplit" | preprocess/bin/mmhsum)
 		found2=""
 		for partdeferredhash2 in $(echo "$deferredhash2" | tr "+" "\n"); do
@@ -64,3 +82,5 @@ zcat $1 | while read line; do
 	rm -rf ${tempwarc}.warc.gz
 	rm -rf $tempsplitsent
 done
+
+rm -rf $tempcache
