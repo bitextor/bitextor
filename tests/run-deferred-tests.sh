0c99ae5313b9d7948c8e5b222cb2c67bc1c43705
elsa <elsa.sar.ali@gmail.com>
2021-07-06 13:54:30 +0200

    refactor deferred tests (1000 character long lines aren't exactly maintainable)

--- tests/run-deferred-tests.sh
+++ tests/run-deferred-tests.sh
@@ -43,9 +43,32 @@ touch "$FAILS"
 # Download necessary files
 # WARCs
 download_warc "${WORK}/data/warc/primeminister.warc.gz" https://github.com/bitextor/bitextor-data/releases/download/bitextor-warc-v1.1/primeminister.warc.gz
+WARC="${WORK}/data/warc/primeminister.warc.gz"
 
 # MT (id >= 10)
-${BITEXTOR} ${FORCE} --notemp --config profiling=True permanentDir="${WORK}/permanent/bitextor-mt-output-en-el" dataDir="${WORK}/data/data-mt-en-el" transientDir="${WORK}/transient-mt-en-el" warcs="['${WORK}/data/warc/primeminister.warc.gz']" preprocessor="warc2text" shards=1 batches=512 lang1=en lang2=el documentAligner="externalMT" alignerCmd="bash ${DIR}/../workflow/example/dummy-translate.sh" sentenceAligner="bleualign" deferred=True tmx=True bifixer=True deduped=True -j ${THREADS} &> "${WORK}/reports/10-mt-en-el.report" && python3 "${BITEXTOR}/deferred-annotation-reconstructor.py" "${WORK}/permanent/bitextor-mt-output-en-el/en-el.deduped.txt.gz" en el "${WORK}/data/warc/primeminister.warc.gz" | bifixer --sdeferredcol 6 --tdeferredcol 7 --ignore_duplicates - - en el  > "${WORK}/outputdeferred" && [ "$(diff ${WORK}/outputdeferred <(zcat ${WORK}/permanent/bitextor-mt-output-en-el/en-el.deduped.txt.gz))" == "" ] ; (status="$?"; nolines=$(zcat -f ${WORK}/outputdeferred | wc -l); annotate_and_echo_info 10 "$status" "$nolines")
+${BITEXTOR} ${FORCE} --notemp \
+  --config profiling=True permanentDir="${WORK}/permanent/bitextor-mt-output-en-el" \
+    dataDir="${WORK}/data/data-mt-en-el" transientDir="${WORK}/transient-mt-en-el" \
+    warcs="['${WARC}']" preprocessor="warc2text" shards=1 batches=512 lang1=en lang2=el \
+    documentAligner="externalMT" alignerCmd="bash ${DIR}/../workflow/example/dummy-translate.sh" \
+    sentenceAligner="bleualign" deferred=True tmx=True bifixer=True deduped=True -j ${THREADS} \
+  &> "${WORK}/reports/10-mt-en-el.report"
+
+BITEXTOR_STATUS=$?
+
+if [ ${BITEXTOR_STATUS} -eq 0 ]; then
+  BITEXTOR_OUTPUT_DEDUPED="${WORK}/permanent/bitextor-mt-output-en-el/en-el.deduped.txt.gz"
+  RECONSTRUCTOR="${DIR}/../deferred-crawling/deferred-annotation-reconstructor.py"
+
+  python3 ${RECONSTRUCTOR} ${BITEXTOR_OUTPUT_DEDUPED} en el ${WARC} \
+    | bifixer -q --sdeferredcol 6 --tdeferredcol 7 --ignore_duplicates - - en el  \
+    > "${WORK}/outputdeferred"
+
+    diff ${WORK}/outputdeferred <(zcat ${BITEXTOR_OUTPUT_DEDUPED}) -q > /dev/null
+    annotate_and_echo_info 10 "$?" "$(wc -l ${WORK}/outputdeferred)"
+else
+  annotate_and_echo_info 10 "${BITEXTOR_STATUS}" "0"
+fi
 
 # Results
 failed=$(cat "$FAILS" | wc -l)
