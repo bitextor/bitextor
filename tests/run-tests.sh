ea743c66db7c8806f24e7a8df7fc4d10cd5a8b3d
Cristian García Romero <cgr71ii@gmail.com>
2021-10-07 11:43:49 +0200

    Tests updated

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -157,10 +157,12 @@ tests-db()
 ### Generate dictionary (id >= 30)
 tests-gendic()
 {
+    wait # wait before starting this test because is very memory-consuming
+
     dic_md5sum_before=$(md5sum "${WORK}/permanent/en-fr.dic" | awk '{print $1}')
     rm -f "${WORK}/permanent/new-en-fr.dic"
 
-    if [[ "$CI" == "true" ]]; then
+    (
         ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
             --config profiling=True permanentDir="${WORK}/permanent/bitextor-gendic-output-en-fr" \
                 dataDir="${WORK}/data/data-gendic-en-fr" transientDir="${WORK}/transient-gendic-en-fr" \
@@ -169,31 +171,22 @@ tests-gendic()
                 initCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/Europarl/Europarl.clipped.en-fr']" \
                 bicleaner=True bicleanerModel="${BICLEANER}/en-fr/en-fr.yaml" bicleanerThreshold=0.1 deferred=False tmx=True \
             &> "${WORK}/reports/30-gendic-en-fr.report"
-        annotate_and_echo_info 30 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendic-output-en-fr/en-fr.sent.gz)"
-        echo "Removing '${WORK}/transient-gendic-en-fr' ($(du -sh ${WORK}/transient-gendic-en-fr | awk '{print $1}'))"
-        rm -rf "${WORK}/transient-gendic-en-fr"
-    else
-        (
-            ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
-                --config profiling=True permanentDir="${WORK}/permanent/bitextor-gendic-output-en-fr" \
-                    dataDir="${WORK}/data/data-gendic-en-fr" transientDir="${WORK}/transient-gendic-en-fr" \
-                    warcs="['${WORK}/data/warc/greenpeace.warc.gz']" preprocessor="warc2text" shards=1 batches=512 lang1=en lang2=fr \
-                    documentAligner="DIC" dic="${WORK}/permanent/new-en-fr.dic" generateDic=True sentenceAligner="hunalign" \
-                    initCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/Europarl/Europarl.clipped.en-fr']" \
-                    bicleaner=True bicleanerModel="${BICLEANER}/en-fr/en-fr.yaml" bicleanerThreshold=0.1 deferred=False tmx=True \
-                &> "${WORK}/reports/30-gendic-en-fr.report"
-            annotate_and_echo_info 30 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendic-output-en-fr/en-fr.sent.gz)"
-        ) &
-    fi
+        annotate_and_echo_info 30 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendic-output-en-fr/en-fr.sent.gz)" #&& \
+        #echo "Removing '${WORK}/transient-gendic-en-fr' ($(du -sh ${WORK}/transient-gendic-en-fr | awk '{print $1}'))" && \
+        #rm -rf "${WORK}/transient-gendic-en-fr"
+    ) &
+
+    wait # wait before finishing the previous test because is very memory-consuming
 }
 
 ### Generate bicleaner model but use existant dictionary (a new dictionary will be generated anyways) (id >= 40)
 tests-genbicleaner()
 {
+    wait # wait before starting this test because is very memory-consuming
+
     rm -f "${BICLEANER}/new/new-en-fr.yaml"
 
-    if [[ "$CI" == "true" ]]; then
-        # Run sequential instead of parallel in order to avoid get run out of memory (GitHub Actions)
+    (
         ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
             --config profiling=True permanentDir="${WORK}/permanent/bitextor-genbicleaner-output-en-fr" \
                 dataDir="${WORK}/data/data-genbicleaner-en-fr" transientDir="${WORK}/transient-genbicleaner-en-fr" \
@@ -205,35 +198,23 @@ tests-genbicleaner()
                 bicleanerThreshold=0.1 deferred=False tmx=True \
             &> "${WORK}/reports/40-genbicleaner-en-fr.report"
         annotate_and_echo_info 40 "$?" "$(get_nolines ${WORK}/permanent/bitextor-genbicleaner-output-en-fr/en-fr.sent.gz)" && \
-            dic_md5sum_after=$(md5sum "${WORK}/permanent/en-fr.dic" | awk '{print $1}')
-        echo "Removing '${WORK}/transient-genbicleaner-en-fr' ($(du -sh ${WORK}/transient-genbicleaner-en-fr | awk '{print $1}'))"
-        rm -rf "${WORK}/transient-genbicleaner-en-fr/"
-    else
-        (
-            ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
-                --config profiling=True permanentDir="${WORK}/permanent/bitextor-genbicleaner-output-en-fr" \
-                    dataDir="${WORK}/data/data-genbicleaner-en-fr" transientDir="${WORK}/transient-genbicleaner-en-fr" \
-                    warcs="['${WORK}/data/warc/greenpeace.warc.gz']" preprocessor="warc2text" shards=1 batches=512 lang1=en lang2=fr \
-                    documentAligner="DIC" dic="${WORK}/permanent/en-fr.dic" generateDic=False sentenceAligner="hunalign" \
-                    initCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/Europarl/Europarl.clipped.en-fr']" \
-                    bicleaner=True bicleanerModel="${BICLEANER}/new/new-en-fr.yaml" bicleanerGenerateModel=True \
-                    bicleanerCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/DGT/DGT.clipped.en-fr']" \
-                    bicleanerThreshold=0.1 deferred=False tmx=True \
-                &> "${WORK}/reports/40-genbicleaner-en-fr.report"
-            annotate_and_echo_info 40 "$?" "$(get_nolines ${WORK}/permanent/bitextor-genbicleaner-output-en-fr/en-fr.sent.gz)" && \
-                dic_md5sum_after=$(md5sum "${WORK}/permanent/en-fr.dic" | awk '{print $1}')
-        ) &
-    fi
+            dic_md5sum_after=$(md5sum "${WORK}/permanent/en-fr.dic" | awk '{print $1}') #&& \
+        #echo "Removing '${WORK}/transient-genbicleaner-en-fr' ($(du -sh ${WORK}/transient-genbicleaner-en-fr | awk '{print $1}'))" && \
+        #rm -rf "${WORK}/transient-genbicleaner-en-fr/"
+    ) &
+
+    wait # wait before finishing the previous test because is very memory-consuming
 }
 
 ### Generate dictionary and bicleaner model (id >= 50)
 tests-gendic-genbicleaner()
 {
+    wait # wait before starting this test because is very memory-consuming
+
     rm -f "${WORK}/permanent/new-new-en-fr.dic"
     rm -f "${BICLEANER}/new-new/new-new-en-fr.yaml"
 
-    if [[ "$CI" == "true" ]]; then
-        # Run sequential instead of parallel in order to avoid get run out of memory (GitHub Actions)
+    (
         ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
             --config profiling=True permanentDir="${WORK}/permanent/bitextor-gendicbicleaner-output-en-fr" \
                 dataDir="${WORK}/data/data-gendicbicleaner-en-fr" transientDir="${WORK}/transient-gendicbicleaner-en-fr" \
@@ -244,24 +225,12 @@ tests-gendic-genbicleaner()
                 bicleanerCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/DGT/DGT.clipped.en-fr']" \
                 bicleanerThreshold=0.1 deferred=False tmx=True \
             &> "${WORK}/reports/50-gendicbicleaner-en-fr.report"
-        annotate_and_echo_info 50 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendicbicleaner-output-en-fr/en-fr.sent.gz)"
-        echo "Removing '${WORK}/transient-gendicbicleaner-en-fr' ($(du -sh ${WORK}/transient-gendicbicleaner-en-fr | awk '{print $1}'))"
-        rm -rf "${WORK}/transient-gendicbicleaner-en-fr"
-    else
-        (
-            ${BITEXTOR} ${FORCE} --notemp -j ${THREADS} \
-                --config profiling=True permanentDir="${WORK}/permanent/bitextor-gendicbicleaner-output-en-fr" \
-                    dataDir="${WORK}/data/data-gendicbicleaner-en-fr" transientDir="${WORK}/transient-gendicbicleaner-en-fr" \
-                    warcs="['${WORK}/data/warc/greenpeace.warc.gz']" preprocessor="warc2text" shards=1 batches=512 lang1=en lang2=fr \
-                    documentAligner="DIC" dic="${WORK}/permanent/new-new-en-fr.dic" generateDic=True sentenceAligner="hunalign" \
-                    initCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/Europarl/Europarl.clipped.en-fr']" \
-                    bicleaner=True bicleanerModel="${BICLEANER}/new-new/new-new-en-fr.yaml" bicleanerGenerateModel=True \
-                    bicleanerCorpusTrainingPrefix="['${WORK}/data/parallel-corpus/DGT/DGT.clipped.en-fr']" \
-                    bicleanerThreshold=0.1 deferred=False tmx=True \
-                &> "${WORK}/reports/50-gendicbicleaner-en-fr.report"
-            annotate_and_echo_info 50 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendicbicleaner-output-en-fr/en-fr.sent.gz)"
-        ) &
-    fi
+        annotate_and_echo_info 50 "$?" "$(get_nolines ${WORK}/permanent/bitextor-gendicbicleaner-output-en-fr/en-fr.sent.gz)" #&& \
+        #echo "Removing '${WORK}/transient-gendicbicleaner-en-fr' ($(du -sh ${WORK}/transient-gendicbicleaner-en-fr | awk '{print $1}'))" && \
+        #rm -rf "${WORK}/transient-gendicbicleaner-en-fr"
+    ) &
+
+    wait # wait before finishing the previous test because is very memory-consuming
 }
 
 # MT and dictionary-based (id >= 60)
