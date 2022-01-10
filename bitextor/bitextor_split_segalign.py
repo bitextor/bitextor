dc2ea1192e33515be252ec7f9f8011a5f28000fc
Cristian García Romero <cgr71ii@gmail.com>
2022-01-10 15:38:54 +0100

    Fix split segalign
    It was not doing splitting at all (i.e. the result was being just
    the renaming of the sentence splitting output)

--- bitextor/bitextor_split_segalign.py
+++ bitextor/bitextor_split_segalign.py
@@ -2,58 +2,51 @@ import argparse
 import gzip
 from sys import stdin
 
-
-def columns(cols):
+def columns(cols, header):
     ranges = []
+    h = header.strip().split('\t')
+
     for c in cols.strip().split(','):
-        start = 1
-        end = 1
-        if c.startswith('-'):
-            start = None
-            end = int(c)
-        elif c.endswith('-'):
-            start = int(c) - 1
-            end = None
-        elif '-' in c:
-            start = c.split('-')[0] - 1
-            end = c.split('-')[1]
-        else:
-            start = int(c) - 1
-            end = int(c)
-        ranges.append((start, end))
+        try:
+            idx = h.index(c)
+
+            ranges.append((idx, idx + 1))
+        except ValueError as e:
+            raise Exception(f"The provided header field '{c}' is not in the header") from e
+
     return ranges
 
+header = next(stdin)
 
 oparser = argparse.ArgumentParser('split file by size of specified fields')
 oparser.add_argument('-s', '--size', default=1024, help="size in MB")
 oparser.add_argument('-o', '--output', default="", help="output files prefix")
-oparser.add_argument('-f', '--fields', type=columns, default="3,4", help="relevant fields")
+oparser.add_argument('-f', '--fields', type=lambda c: columns(c, header), default="3,4", help="relevant fields")
 oparser.add_argument('--gzip', action="store_true", default=False, help="compress the output")
 options = oparser.parse_args()
 
 output_file = None
-max_size = int(options.size) * 1024 * 1024
+max_size = int(options.size) * 1024 * 1024 # Bytes
 file_n = 0
 current_size = max_size
 
-if output_file is not None:
-    output_file.close()
-if options.gzip:
-    output_file = gzip.open(f'{options.output}{file_n}.gz', 'tw')
-else:
-    output_file = open(f'{options.output}{file_n}', 'w')
-
 for line in stdin:
     fields = line.strip().split('\t')
-    if current_size >= max_size and file_n > 0:
+
+    if current_size >= max_size:
         if output_file is not None:
             output_file.close()
         if options.gzip:
             output_file = gzip.open(f'{options.output}{file_n}.gz', 'tw')
         else:
             output_file = open(f'{options.output}{file_n}', 'w')
+
+        output_file.write(header)
+
         file_n = file_n + 1
         current_size = 0
+
     for (start, end) in options.fields:
         current_size += len(' '.join(fields[start:end]))
+
     output_file.write(line)
