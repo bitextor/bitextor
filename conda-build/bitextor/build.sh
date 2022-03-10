79ff99113c8a59e8463e648aaf03d0b4406ddc88
Cristian García Romero <cgr71ii@gmail.com>
2022-03-10 10:09:43 +0100

    Fix conda build

--- conda-build/bitextor/build.sh
+++ conda-build/bitextor/build.sh
@@ -16,8 +16,8 @@ export PIP_NO_INDEX="False" # We are downloading requisites from PyPi
 export PIP_NO_DEPENDENCIES="False" # We need the dependencies from our defined dependencies
 export PIP_IGNORE_INSTALLED="False" # We need to take into account the dependencies
 
-if [[ ! -f $CONDA_PREFIX/lib/libhunspell.so ]]; then
-  ln -s $CONDA_PREFIX/lib/libhunspell-1.7.so $CONDA_PREFIX/lib/libhunspell.so
+if [[ ! -f $PREFIX/lib/libhunspell.so ]]; then
+  ln -s $PREFIX/lib/libhunspell-1.7.so $PREFIX/lib/libhunspell.so
 fi
 
 pip3 install .
