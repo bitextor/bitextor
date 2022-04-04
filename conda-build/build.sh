b721f374cb93fca1007644a964897007f6978862
Cristian García Romero <cgr71ii@gmail.com>
2022-04-04 13:05:49 +0200

    Update conda build scripts

--- conda-build/build.sh
+++ conda-build/build.sh
@@ -5,46 +5,27 @@ pushd "bitextor" > /dev/null
 
 # Go dependencies
 echo -e " - \e[4mInstalling Go dependencies...\e[0m"
-mkdir "$PWD/../gopath"
-go env
-export GOPATH="$PWD/../gopath"
+mkdir "$SRC_DIR/../gopath"
+export GOPATH="$SRC_DIR/../gopath"
 go get github.com/paracrawl/giashard/...
 
 # Pip dependencies
-echo -e " - \e[4mInstalling python3 dependencies...\e[0m"
+echo -e " - \e[4mInstalling python dependencies...\e[0m"
 export PIP_NO_INDEX="False" # We are downloading requisites from PyPi
 export PIP_NO_DEPENDENCIES="False" # We need the dependencies from our defined dependencies
 export PIP_IGNORE_INSTALLED="False" # We need to take into account the dependencies
 
-if [[ ! -f $PREFIX/lib/libhunspell.so ]]; then
-  ln -s $PREFIX/lib/libhunspell{-1.7,}.so
-fi
-if [[ ! -f $PREFIX/lib/libhunspell.a ]]; then
-  ln -s $PREFIX/lib/libhunspell{-1.7,}.a
-fi
-
-pip3 install .[all]
-### FastSpell (bicleaner-hardrules)
-INCLUDE_PATH="$PREFIX/include" pip3 install hunspell
-### Bicleaner, Bicleaner AI and KenLM
-pip3 install ./bicleaner
-pip3 install ./bicleaner-ai
-pip3 install ./kenlm --install-option="--max_order 7"
-### Bifixer
-pip3 install ./bifixer
-### Biroamer and model
-pip3 install ./biroamer && \
-python3 -c "from flair.models import SequenceTagger; SequenceTagger.load('flair/ner-english-fast')"
-### CLD3
-pip3 install Cython
-pip3 install pycld3
-### Linguacrawl
-pip3 install git+https://github.com/transducens/linguacrawl.git
+$PYTHON -m pip install .[all]
+## CLD3
+$PYTHON -m pip install Cython
+$PYTHON -m pip install pycld3
 
 echo -e " - \e[4mMake...\e[0m"
 mkdir -p build && cd build
-CPATH="$PREFIX/include:$CPATH" cmake "-DCMAKE_INSTALL_PREFIX=${PREFIX}" ..
-CPATH="$PREFIX/include:$CPATH" make -j install
+CPATH="$PREFIX/include:$CPATH" cmake \
+  "-DCMAKE_INSTALL_PREFIX=$PREFIX" -DSKIP_BIROAMER=ON -DSKIP_KENLM=ON \
+  ..
+CPATH="$PREFIX/include:$CPATH" LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH" make -j install
 
 popd > /dev/null
 
@@ -56,16 +37,20 @@ find . -type d -or -type l | grep /__pycache__$ | xargs -I{} rm -rf {}
 find . -type f -or -type l | grep [.]o$ | xargs -I{} rm -rf {}
 find . -type d -or -type l | grep /CMakeFiles$ | xargs -I{} rm -rf {}
 
-# Copy src (not usual in builds, but we need src because we are using bash and python scripts)
-cp -r "$SRC_DIR/bitextor" "$PREFIX"
-cp -r "$SRC_DIR/gopath" "$PREFIX"
+# Copy python scripts, necessary data and 3rd packages
+mkdir -p "$PREFIX/bitextor"
+
+cp -r "$SRC_DIR/bitextor/bitextor" "$PREFIX/bitextor" # Scripts and data
+cp -r "$SRC_DIR/../gopath" "$PREFIX"
 cp -r "$SRC_DIR/heritrix-3.4.0-SNAPSHOT" "$PREFIX"
 
-# Soft links (is necessary that is relative, not absolute)
+# Soft links (necessary to be relative, not absolute)
 pushd "$PREFIX/bin" > /dev/null
 
-ln -s ../gopath/bin/giashard ./giashard || echo -e " - \e[31m\e[4mpost-build-act: could not create link 'giashard'...\e[0m" # Warning: bitextor will look for giashard at ~/go/bin
-ln -s "$PREFIX/heritrix-3.4.0-SNAPSHOT" "$PREFIX/heritrix3" || echo -e " - \e[31m\e[4mpost-build-act: could not create link 'heritrix3' (directory)...\e[0m"
+ln -s ../gopath/bin/giashard ./giashard || \
+  >&2 echo -e " - \e[31m\e[4mpost-build-act: could not create link 'giashard'...\e[0m"
+ln -s "$PREFIX/heritrix-3.4.0-SNAPSHOT" "$PREFIX/heritrix3" || \
+  >&2 echo -e " - \e[31m\e[4mpost-build-act: could not create link 'heritrix3' (directory)...\e[0m"
 
 # Restore initial working directory (trying to prevent errors)
 popd > /dev/null
--- conda-build/make_build.sh
+++ conda-build/make_build.sh
@@ -1,45 +1,56 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PACKAGE_VERBOSE_NAME="Bicleaner AI"
 
 usage()
 {
-  echo "$(basename $0) [-h] [-e <CONDA_ENV_NAME>] [-r] -n <package_name>"
+  echo "$(basename $0) [-h] [-e <conda_env_name>] [-r] [-s] -n <package_name> -p <python_version>"
   echo ""
   echo "OPTIONS:"
-  echo "  -e <CONDA_ENV_NAME>     Name of the conda environment which will be used to build"
-  echo "                          Bitextor. If not specified, the default value is"
+  echo "  -h                      It displays this help message."
+  echo "  -e <conda_env_name>     Name of the conda environment which will be used to build"
+  echo "                          ${PACKAGE_VERBOSE_NAME}. If not specified, the default value is"
   echo "                          the provided package name with the suffix '-build'."
   echo "  -r                      It removes the conda environment before start if exists."
-  echo "  -h                      It displays this help message."
-  echo "  -n                      Conda package name in order to retrieve the pkg path"
+  echo "  -s                      Skip untracked files check."
+  echo "  -n <package_name>       Conda package name in order to retrieve the pkg path"
+  echo "  -p <python_version>     Python version to be used to build ${PACKAGE_VERBOSE_NAME}."
 }
 
 CONDA_ENV_NAME=""
 CONDA_PACKAGE_NAME=""
 REMOVE_ENV_IF_EXISTS=""
+CONDA_PYTHON_VERSION=""
+SKIP_UNTRACKED_FILES_CHECK=""
 
-while getopts "e:n:rh" options
+while getopts "e:n:p:rsh" options
 do
   case "${options}" in
-    e) CONDA_ENV_NAME=$OPTARG;;
-    r) REMOVE_ENV_IF_EXISTS="y";;
     h) usage
        exit 0;;
+    e) CONDA_ENV_NAME=$OPTARG;;
+    r) REMOVE_ENV_IF_EXISTS="y";;
+    s) SKIP_UNTRACKED_FILES_CHECK="y";;
     n) CONDA_PACKAGE_NAME=$OPTARG;;
+    p) CONDA_PYTHON_VERSION=$OPTARG;;
     \?) usage 1>&2
         exit 1;;
   esac
 done
 
-if [[ "$CONDA_PACKAGE_NAME" == "" ]]; then
-  >&2 echo "Error: package name is mandatory"
+# Info
+echo "git describe --tags: $(git describe --tags || true)"
+echo "git describe --always: $(git describe --always)"
+
+if [[ -z "$CONDA_PACKAGE_NAME" ]] || [[ -z "$CONDA_PYTHON_VERSION" ]]; then
+  >&2 echo "Error: not all mandatory parameters were provided"
   >&2 echo ""
   usage 1>&2
   exit 1
 fi
 
-if [[ "$CONDA_ENV_NAME" == "" ]]; then
+if [[ -z "$CONDA_ENV_NAME" ]]; then
   CONDA_ENV_NAME="${CONDA_PACKAGE_NAME}-build"
 fi
 
@@ -50,29 +61,41 @@ if [[ ! -f "${CONDA_PACKAGE_PATH}/meta.yaml" ]]; then
   exit 1
 fi
 
+BASE_PACKAGE_PATH="${SCRIPT_DIR}/.."
+UNTRACKED_FILES=$(git ls-files "$BASE_PACKAGE_PATH" --ignored --exclude-standard --others | wc -l)
+CHANGED_FILES=$(git diff --name-only | wc -l)
+
+if [[ "$UNTRACKED_FILES" -ne "0" ]]; then
+  if [[ -z "$SKIP_UNTRACKED_FILES_CHECK" ]]; then
+    >&2 echo "Error: there are $UNTRACKED_FILES untracked files in the repository"
+    exit 1
+  else
+    >&2 echo "Warning: there are $UNTRACKED_FILES untracked files in the repository"
+  fi
+fi
+
+if [[ "$CHANGED_FILES" -ne "0" ]]; then
+  >&2 echo "Warning: there are $CHANGED_FILES changed files in the repository"
+fi
+
 source $(conda info --base)/etc/profile.d/conda.sh
 
-if [[ "$REMOVE_ENV_IF_EXISTS" == "y" ]]; then
+if [[ -n "$REMOVE_ENV_IF_EXISTS" ]]; then
   conda remove -y -n $CONDA_ENV_NAME --all
 fi
 
-if [[ "$(conda env list | grep ^$CONDA_ENV_NAME[\ +])" != "" ]]; then
+if [[ -n "$(conda env list | grep ^$CONDA_ENV_NAME[\ +])" ]]; then
   >&2 echo "Error: conda environment '$CONDA_ENV_NAME' already exists"
   >&2 echo ""
   usage 1>&2
   exit 1
 fi
 
-conda create -y -n $CONDA_ENV_NAME -c conda-forge conda-build conda-verify
+conda create -y -n $CONDA_ENV_NAME -c conda-forge conda-build conda-verify python=$CONDA_PYTHON_VERSION
 conda activate $CONDA_ENV_NAME
+conda update -y conda
 
-echo "git describe --tags:"
-git describe --tags || true
-
-echo "git describe --always:"
-git describe --always
-
-CONDA_CHANNELS="-c conda-forge -c bioconda -c dmnapolitano -c esarrias"
+CONDA_CHANNELS="-c conda-forge -c bitextor -c bioconda -c dmnapolitano -c esarrias"
 
 # Make build
-conda-build --no-anaconda-upload --no-test $CONDA_CHANNELS $CONDA_PACKAGE_PATH
+conda-build --no-test --no-anaconda-upload $CONDA_CHANNELS $CONDA_PACKAGE_PATH --python=$CONDA_PYTHON_VERSION
