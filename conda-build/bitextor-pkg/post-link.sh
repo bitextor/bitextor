56b0026043baefaa5098a4dcd96214b1bbb9d6b0
aarongaliano <agj32@alu.ua.es>
2022-11-29 08:55:36 +0100

    Created symbolic links

--- conda-build/bitextor-pkg/post-link.sh
+++ conda-build/bitextor-pkg/post-link.sh
@@ -1,9 +1,1 @@
-
-echo -e "\n" >> "$PREFIX/.messages.txt"
-
-# Create Heritrix3 soft link after the installation and inform the user about the location
-if [[ ! -d "$PREFIX/heritrix3" ]]; then
-  echo "Could not find Heritrix3 in '$PREFIX/heritrix3' (not installed?)" >> "$PREFIX/.messages.txt"
-else
-  echo "You may find Heritrix3 installation in '\$CONDA_PREFIX/heritrix3'" >> "$PREFIX/.messages.txt"
-fi
+../post-link.sh
